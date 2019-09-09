1c583b626676a613b95b46563155e55f50a2d44e
david <david@insighteyes.com>
2019-09-09 21:39:38 +0800

    david modify vcs code @ sugar 0909 c

---
+++ _2.vcs/my_vcs_lesson_5/_ReadWriteFile/vcs_ReadWrite_HTML/vcs_ReadWrite_HTML/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;   //for WebRequest
+using System.IO;    //for Stream
+
+namespace vcs_ReadWrite_HTML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //使用 WebBrowser 取得網頁原始碼
+            string txtUrl = @"https://www.google.com.tw/";  //預設網址
+            //string txtUrl = @"C:/My_Link.html";
+
+            this.webBrowser1.Navigate(txtUrl);  // WebBrowser1 網頁載入
+
+            // 使用 WebBrowser.DocumentStream 取得網頁內容
+            // 使用 StreamReader 讀入資料流，設定編碼為 Encoding.Default
+            //System.IO.StreamReader getReader = new System.IO.StreamReader(this.webBrowser1.DocumentStream, System.Text.Encoding.Default);
+            //string gethtml = getReader.ReadToEnd();
+            string htmlB = webBrowser1.DocumentText;
+            this.richTextBox1.Text += htmlB;
+
+        }
+
+        public static string myUrl = "https://www.google.com.tw/";    //要抓取的網頁
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //讀取網頁的內容
+            string data = getContent(myUrl);
+            richTextBox1.Text += "data:" + data + "\n";
+
+        }
+        private string getContent(string Url)
+        {
+            string strResult = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            //聲明一個HttpWebRequest請求
+            request.Timeout = 30000;
+            //設置連接逾時時間
+            request.Headers.Set("Pragma", "no-cache");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream streamReceive = response.GetResponseStream();
+            Encoding encoding = Encoding.GetEncoding("BIG5");
+            StreamReader streamReader = new StreamReader(streamReceive, encoding);	//Encoding.Default解決讀取一般編碼檔案中文字錯亂的問題
+            strResult = streamReader.ReadToEnd();
+            streamReader.Close();
+            return strResult;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //C#,從網頁上下載檔案範例
+            string url = "http://blog.darkthread.net/images/darkthreadbanner.gif";
+            HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+
+            System.IO.Stream dataStream = httpResponse.GetResponseStream();
+            byte[] buffer = new byte[8192];
+
+            //FileStream fs = new FileStream("X:\\TEMP\\Darkthread.gif", FileMode.Create, FileAccess.Write);
+            FileStream fs = new FileStream("C:\\______test_vcs\\aaaaaaaa.gif", FileMode.Create, FileAccess.Write);
+            int size = 0;
+            do
+            {
+                size = dataStream.Read(buffer, 0, buffer.Length);
+                if (size > 0)
+                    fs.Write(buffer, 0, size);
+            } while (size > 0);
+            fs.Close();
+            httpResponse.Close();
+
+            richTextBox1.Text += "Done at " + DateTime.Now.ToString("HH:mm:ss.fff") + "\n";
+        }
+
+    }
+}
---
+++ _2.vcs/my_vcs_lesson_5/_ReadWriteFile/vcs_ReadWrite_INI/vcs_ReadWrite_INI/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+
+namespace vcs_ReadWrite_INI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string _FilePath = "c:\\______test_vcs\\sample.ini";
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern bool WritePrivateProfileString(
+        string lpAppName, string lpKeyName, string lpString, string lpFileName);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern int GetPrivateProfileString(
+        string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString,
+        int nSize, string lpFileName);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern int GetPrivateProfileInt(
+        string lpAppName, string lpKeyName, int lpDefault, string lpFileName);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "write data to test_ini.ini.\n";
+            WritePrivateProfileString("Demo", "abc", "123", "c:\\______test_vcs\\test_ini.ini");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "read data from test_ini.ini.\n";
+            StringBuilder temp = new StringBuilder();
+            GetPrivateProfileString("Demo", "abc", "", temp, 255, "c:\\______test_vcs\\test_ini.ini");
+            richTextBox1.Text += "get value = " + temp + "\n";
+
+            //Console.WriteLine(temp);
+            //Console.ReadLine();
+
+
+        }
+
+        /// <summary>
+        /// 取得 Key 相對的 Value 值，若沒有則使用預設值(DefaultValue)。
+        /// </summary>
+        /// <param name="Section">Section。</param>
+        /// <param name="Key">Key。</param>
+        /// <param name="DefaultValue">DefaultValue。</param>        
+        public string getKeyValue(string Section, string Key, string DefaultValue)
+        {
+            StringBuilder sbResult = null;
+            try
+            {
+                sbResult = new StringBuilder(255);
+                GetPrivateProfileString(Section, Key, "", sbResult, 255, this._FilePath);
+                return (sbResult.Length > 0) ? sbResult.ToString() : DefaultValue;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "write data to student.ini.\n";
+            string strName, strTemp;
+            int nAge;
+            strName = "張三";
+            nAge = 12;
+            WritePrivateProfileString("StudentInfo", "Name", strName, "c://______test_vcs//student.ini");
+            WritePrivateProfileString("StudentInfo", "Age", nAge.ToString(), "c://______test_vcs//student.ini");
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "read data from student.ini.\n";
+
+
+
+
+
+        }
+
+    }
+}
