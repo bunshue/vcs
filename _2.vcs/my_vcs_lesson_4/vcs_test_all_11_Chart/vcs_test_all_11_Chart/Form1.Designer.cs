7fcd96583ff4a17db264516703e48d9da098720a
david <david@insighteyes.com>
2020-01-14 13:51:52 +0800

    david modify vcs test code.
    @sugar 0114a

---
+++ _2.vcs/my_vcs_lesson_4/vcs_test_all_11_Chart/vcs_test_all_11_Chart/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace vcs_test_all_11_Chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "plotChart1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(950, 352);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 425);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "plotChart2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 481);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "plotChart3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(149, 374);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 34);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "plotChart4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(149, 425);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 34);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "plotChart5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(849, 374);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 34);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "clear";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(149, 481);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(101, 34);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "plotChart6";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 552);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ _2.vcs/my_vcs_lesson_4/vcs_test_all_11_Chart/vcs_test_all_11_Chart/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;  //for Series
+
+namespace vcs_test_all_11_Chart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            plotChart1();
+
+        }
+
+        private void plotChart1() //繪圖
+        {
+
+            int[,] array = new int[,] {
+            {1,8,9,7,105,11,50,999,500,1},
+            {12,15,11,18,733,5,4,3,2,500} };
+
+
+            //標題 最大數值
+            Series series1 = new Series("第一條線", 1000);
+            Series series2 = new Series("第二條線", 1000);
+
+            //設定線條顏色
+            series1.Color = Color.Blue;
+            series2.Color = Color.Red;
+
+            //設定字型
+            series1.Font = new System.Drawing.Font("新細明體", 14);
+            series2.Font = new System.Drawing.Font("標楷體", 12);
+
+            //折線圖
+            series1.ChartType = SeriesChartType.Line;
+            series2.ChartType = SeriesChartType.Line;
+
+            //將數值顯示在線上
+            series1.IsValueShownAsLabel = true;
+            series2.IsValueShownAsLabel = true;
+
+
+            //將數值新增至序列
+            for (int index = 0; index < 10; index++)
+            {
+                series1.Points.AddXY(index, array[0, index]);
+                series2.Points.AddXY(index, array[1, index]);
+            }
+
+            //將序列新增到圖上
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+
+            //標題
+            this.chart1.Titles.Add("標題");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            plotChart2();
+
+        }
+
+        private void plotChart2() //繪圖
+        {
+            string[] xValues = { "一月", "二月", "三月", "四月", "五月", "六月", "七月" };
+            int[] yValues = { 25, 18, 30, 24, 35, 50, 40 };
+            var objSeries = chart1.Series.First();
+            objSeries.Points.DataBindXY(xValues, yValues);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            plotChart3();
+
+        }
+
+        private void plotChart3() //繪圖
+        {
+            //---------------------
+            chart1.Series.Clear();  //每次使用此function前先清除圖表
+            Series series1 = new Series("Di0", 500); //初始畫線條(名稱，最大值)
+            series1.Color = Color.Blue; //設定線條顏色
+            series1.Font = new System.Drawing.Font("新細明體", 10); //設定字型
+            series1.ChartType = SeriesChartType.Line; //設定線條種類
+            chart1.ChartAreas[0].AxisY.Minimum = 0;//設定Y軸最小值
+            chart1.ChartAreas[0].AxisY.Maximum = 500;//設定Y軸最大值
+            //chart1.ChartAreas[0].AxisY.Enabled= AxisEnabled.False; //隱藏Y 軸標示
+            //chart1.ChartAreas[0].AxisY.MajorGrid.Enabled= true;  //隱藏Y軸標線
+            series1.IsValueShownAsLabel = true; //是否把數值顯示在線上
+            //把值加入X 軸Y 軸
+            series1.Points.AddXY("A", 200);
+            series1.Points.AddXY("B", 100);
+            series1.Points.AddXY("C", 100);
+            series1.Points.AddXY("D", 30);
+            series1.Points.AddXY("E", 300);
+            series1.Points.AddXY("F", 70);
+            this.chart1.Series.Add(series1);//將線畫在圖上
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            plotChart4();
+
+        }
+
+        private void plotChart4() //繪圖
+        {
+            //Bar example
+            this.chart1.Series.Clear();
+
+            // Data arrays
+            string[] seriesArray = { "Cat", "Dog", "Bird", "Monkey" };
+            int[] pointsArray = { 2, 1, 7, 5 };
+
+            // Set palette
+            this.chart1.Palette = ChartColorPalette.EarthTones;
+
+            // Set title
+            this.chart1.Titles.Add("Animals");
+
+            // Add series.
+            for (int i = 0; i < seriesArray.Length; i++)
+            {
+                Series series = this.chart1.Series.Add(seriesArray[i]);
+                series.Points.Add(pointsArray[i]);
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            plotChart5();
+
+        }
+
+        private void plotChart5() //繪圖
+        {
+            //SplineChartExample
+            this.chart1.Series.Clear();
+
+            this.chart1.Titles.Add("Total Income");
+
+            Series series = this.chart1.Series.Add("Total Income");
+            series.ChartType = SeriesChartType.Spline;
+            series.Points.AddXY("September", 100);
+            series.Points.AddXY("Obtober", 300);
+            series.Points.AddXY("November", 800);
+            series.Points.AddXY("December", 200);
+            series.Points.AddXY("January", 600);
+            series.Points.AddXY("February", 400);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //clear
+
+        }
+
+
+        Timer timer;
+        double x;
+        bool isRunning = false;
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (isRunning == false)
+            {
+                isRunning = true;
+                timer = new Timer();
+                timer.Tick += Timer_Tick;
+                timer.Interval = 50;
+                timer.Start();
+            }
+            else
+            {
+                isRunning = false;
+                timer.Stop();
+
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.AddXY(x, 3 * Math.Sin(5 * x) + 5 * Math.Cos(3 * x));
+
+            if (chart1.Series[0].Points.Count > 100)
+                chart1.Series[0].Points.RemoveAt(0);
+
+            chart1.ChartAreas[0].AxisX.Minimum = chart1.Series[0].Points[0].XValue;
+            chart1.ChartAreas[0].AxisX.Maximum = x;
+
+            x += 0.1;
+        }
+
+
+    }
+}
