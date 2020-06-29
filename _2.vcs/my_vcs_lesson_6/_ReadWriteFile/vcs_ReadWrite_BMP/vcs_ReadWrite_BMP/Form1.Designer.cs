1fa158528e5c20eb819dbb5be98103b0c4a00438
david <david@insighteyes.com>
2020-06-29 20:34:22 +0800

    david modify vcs code for bmp file write.
    @sugar 0629

--- _2.vcs/my_vcs_lesson_6/_ReadWriteFile/vcs_ReadWrite_BMP/vcs_ReadWrite_BMP/Form1.Designer.cs
+++ _2.vcs/my_vcs_lesson_6/_ReadWriteFile/vcs_ReadWrite_BMP/vcs_ReadWrite_BMP/Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -65,7 +68,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button2.Location = new System.Drawing.Point(12, 90);
+            this.button2.Location = new System.Drawing.Point(12, 208);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(126, 41);
             this.button2.TabIndex = 3;
@@ -73,11 +76,39 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(12, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 41);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "製作BMP檔_色階";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(12, 143);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 41);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "色階圖寫字";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1148, 659);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.pictureBox1);
@@ -95,6 +126,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- _2.vcs/my_vcs_lesson_6/_ReadWriteFile/vcs_ReadWrite_BMP/vcs_ReadWrite_BMP/Form1.cs
+++ _2.vcs/my_vcs_lesson_6/_ReadWriteFile/vcs_ReadWrite_BMP/vcs_ReadWrite_BMP/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 
 using System.IO;    //for File
+using System.Drawing.Imaging;   //for ImageFormat
 
 namespace vcs_ReadWrite_BMP
 {
@@ -16,6 +17,7 @@ namespace vcs_ReadWrite_BMP
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -194,6 +196,215 @@ namespace vcs_ReadWrite_BMP
             richTextBox1.Text += "P W : " + img.PhysicalDimension.Width.ToString() + "\n";
             richTextBox1.Text += "P H : " + img.PhysicalDimension.Height.ToString() + "\n";
             pictureBox1.Image = img;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i;
+            int j;
+            int ww = 256 * 6 + 48;
+            int hh = 300;
+            int size = ww * hh * 4 + 0x36;
+            int h_res = 0x0EC4;
+            int v_res = 0x0EC4;
+
+            byte[] data = new byte[size];
+
+            for (i = 0; i < size; i++)
+            {
+                data[i] = 0;
+            }
+            data[0] = (byte)'B';
+            data[1] = (byte)'M';
+            data[2] = (byte)(size % 256);
+            data[3] = (byte)((size / 256) % 256);
+            data[4] = (byte)((size / 256 / 256) % 256);
+            data[5] = (byte)((size / 256 / 256) % 256);
+            data[10] = 0x36;
+            data[14] = 0x28;
+
+            data[18] = (byte)(ww % 256);
+            data[19] = (byte)((ww / 256) % 256);
+            data[20] = (byte)((ww / 256 / 256) % 256);
+            data[21] = (byte)((ww / 256 / 256) % 256);
+
+            data[22] = (byte)(hh % 256);
+            data[23] = (byte)((hh / 256) % 256);
+            data[24] = (byte)((hh / 256 / 256) % 256);
+            data[25] = (byte)((hh / 256 / 256) % 256);
+
+            data[26] = 0x01;
+            data[28] = 0x20;
+
+            data[38] = (byte)(h_res % 256);
+            data[39] = (byte)((h_res / 256) % 256);
+            data[40] = (byte)((h_res / 256 / 256) % 256);
+            data[41] = (byte)((h_res / 256 / 256) % 256);
+
+            data[42] = (byte)(v_res % 256);
+            data[43] = (byte)((v_res / 256) % 256);
+            data[44] = (byte)((v_res / 256 / 256) % 256);
+            data[45] = (byte)((v_res / 256 / 256) % 256);
+
+            for (j = 0; j < hh; j++)
+            {
+                for (i = 0; i < ww; i++)
+                {
+                    data[54 + ww * 4 * j + i * 4 + 0] = (byte)(i / 6);    //B
+                    data[54 + ww * 4 * j + i * 4 + 1] = (byte)(i / 6);    //G
+                    data[54 + ww * 4 * j + i * 4 + 2] = (byte)(i / 6);    //R
+                    data[54 + ww * 4 * j + i * 4 + 3] = 0xFF;       //A
+                }
+            }
+
+            for (j = 0; j < hh; j++)
+            {
+                for (i = 0; i < ww; i++)
+                {
+                    if ((j == 0) || (j == (hh - 1)))
+                    {
+                        data[54 + ww * 4 * j + i * 4 + 0] = 0;   //B
+                        data[54 + ww * 4 * j + i * 4 + 1] = 0;   //G
+                        data[54 + ww * 4 * j + i * 4 + 2] = 255;   //R
+                        data[54 + ww * 4 * j + i * 4 + 3] = 0xFF;   //A
+                    }
+                    else if ((i == 0) || (i == (ww - 1)))
+                    {
+                        data[54 + ww * 4 * j + i * 4 + 0] = 0;   //B
+                        data[54 + ww * 4 * j + i * 4 + 1] = 0;   //G
+                        data[54 + ww * 4 * j + i * 4 + 2] = 255;   //R
+                        data[54 + ww * 4 * j + i * 4 + 3] = 0xFF;   //A
+                    }
+                    else if ((i % 48) == 47)
+                    {
+                        data[54 + ww * 4 * j + i * 4 + 0] = 0;   //B
+                        data[54 + ww * 4 * j + i * 4 + 1] = 0;   //G
+                        data[54 + ww * 4 * j + i * 4 + 2] = 255;   //R
+                        data[54 + ww * 4 * j + i * 4 + 3] = 0xFF;   //A
+                    }
+                    else if (i > 256 * 6)
+                    {
+                        data[54 + ww * 4 * j + i * 4 + 0] = 255;   //B
+                        data[54 + ww * 4 * j + i * 4 + 1] = 255;   //G
+                        data[54 + ww * 4 * j + i * 4 + 2] = 255;   //R
+                        data[54 + ww * 4 * j + i * 4 + 3] = 0xFF;   //A
+                    }
+                    else
+                    {
+                        data[54 + ww * 4 * j + i * 4 + 0] = (byte)((i / 6) / 8 * 8);    //B
+                        data[54 + ww * 4 * j + i * 4 + 1] = (byte)((i / 6) / 8 * 8);    //G
+                        data[54 + ww * 4 * j + i * 4 + 2] = (byte)((i / 6) / 8 * 8);    //R
+                        data[54 + ww * 4 * j + i * 4 + 3] = 0xFF;       //A
+                    }
+                }
+            }
+
+            /*
+            打印資料
+            string data_read_result = string.Empty;
+            foreach (byte b in data)
+            {
+                data_read_result += b.ToString("X2");
+            }
+            richTextBox1.Text += data_read_result;
+            */
+
+            string filename = Application.StartupPath + "\\bmp_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".bmp";
+
+            //寫資料
+            File.WriteAllBytes(filename, data);
+            richTextBox1.Text += "\n製作BMP檔\t" + filename + "\n";
+
+            return;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Graphics g;
+            Pen p;
+            SolidBrush sb;
+            Bitmap bitmap1;
+            string FileName = "";
+            Color foreground_color = Color.Red;
+            Color background_color = Color.White;
+
+            p = new Pen(foreground_color, 3);
+            sb = new SolidBrush(foreground_color);
+
+
+            openFileDialog1.Filter = "圖片(*.bmp,*.jpg,*.png)|*.bmp;*.jpg;*.png";
+            //openFileDialog1.Filter = "BMP|*.bmp|JPG|*.jpg|PNG|*.png|GIF|*.gif";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileName = this.openFileDialog1.FileName.Trim();
+                richTextBox1.Text += FileName + "\n";
+                pictureBox1.ImageLocation = openFileDialog1.FileName;
+            }
+            else
+            {
+                richTextBox1.Text += "未選取檔案\n";
+
+                return;
+            }
+
+            int width;
+            int height;
+
+            //string filename = "C:\\______test_files\\step2.png";
+            ///richTextBox1.Text += "開啟檔案: " + filename + ", 並顯示之\n";
+
+            bitmap1 = new Bitmap(FileName);
+            //bitmap1 = new Bitmap(pictureBox1.Image);
+
+            richTextBox1.Text += "W = " + bitmap1.Width.ToString() + " H = " + bitmap1.Height.ToString() + "\n";
+
+            width = bitmap1.Width;
+            height = bitmap1.Height;
+
+            //pictureBox1.Size = new Size(1, 1);
+            pictureBox1.Size = new Size(width, height);
+            pictureBox1.Location = new Point(0, 0);
+
+            g = Graphics.FromImage(bitmap1);
+
+            int xx;
+            int yy;
+            int ss;
+
+            Font f;
+            f = new Font("Arial", 15);
+            for (xx = 0; xx < width; xx += 48)
+            {
+                ss = (xx / 48) * 8;
+                if (ss == 256)
+                    ss = 255;
+                g.DrawString(ss.ToString(), f, sb, new PointF(xx, 20));
+            }
+
+            //g.DrawRectangle(p, new Rectangle(0, 0, width - 1, height - 1));
+
+            pictureBox1.Image = bitmap1;
+
+            if (bitmap1 != null)
+            {
+                string filename = Application.StartupPath + "\\IMG_" + DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                String filename1 = filename + ".jpg";
+                String filename2 = filename + ".bmp";
+                String filename3 = filename + ".png";
+
+                bitmap1.Save(@filename1, ImageFormat.Jpeg);
+                bitmap1.Save(@filename2, ImageFormat.Bmp);
+                bitmap1.Save(@filename3, ImageFormat.Png);
+
+                richTextBox1.Text += "存檔成功\n";
+                richTextBox1.Text += "已存檔 : " + filename1 + "\n";
+                richTextBox1.Text += "已存檔 : " + filename2 + "\n";
+                richTextBox1.Text += "已存檔 : " + filename3 + "\n";
+            }
+            else
+                richTextBox1.Text += "無圖可存\n";
+
+
         }
     }
 }
