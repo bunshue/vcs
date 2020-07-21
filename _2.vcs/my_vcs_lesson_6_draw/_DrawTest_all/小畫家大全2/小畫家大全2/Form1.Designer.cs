2ed79106a470442ef73a04a4a1cb3a7c76ed82be
david <david@insighteyes.com>
2020-07-21 19:56:44 +0800

    david modify vcs code for drawing
    @sugar

--- _2.vcs/my_vcs_lesson_6_draw/_DrawTest_all/小畫家大全2/小畫家大全2/Form1.Designer.cs
+++ _2.vcs/my_vcs_lesson_6_draw/_DrawTest_all/小畫家大全2/小畫家大全2/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace 小畫家大全
+﻿namespace 小畫家大全2
 {
     partial class Form1
     {
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.radioButton6 = new System.Windows.Forms.RadioButton();
             this.radioButton5 = new System.Windows.Forms.RadioButton();
@@ -39,11 +40,19 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(767, 53);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(192, 504);
+            this.richTextBox1.TabIndex = 21;
+            this.richTextBox1.Text = "";
             // 
             // groupBox1
             // 
@@ -56,7 +65,7 @@
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(418, 57);
-            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabIndex = 20;
             this.groupBox1.TabStop = false;
             // 
             // radioButton6
@@ -68,7 +77,6 @@
             this.radioButton6.TabIndex = 5;
             this.radioButton6.Text = "Eraser";
             this.radioButton6.UseVisualStyleBackColor = true;
-            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton5
             // 
@@ -79,7 +87,6 @@
             this.radioButton5.TabIndex = 4;
             this.radioButton5.Text = "String";
             this.radioButton5.UseVisualStyleBackColor = true;
-            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton4
             // 
@@ -90,7 +97,6 @@
             this.radioButton4.TabIndex = 3;
             this.radioButton4.Text = "Circle";
             this.radioButton4.UseVisualStyleBackColor = true;
-            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton3
             // 
@@ -101,7 +107,6 @@
             this.radioButton3.TabIndex = 2;
             this.radioButton3.Text = "Rectangle";
             this.radioButton3.UseVisualStyleBackColor = true;
-            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton2
             // 
@@ -112,7 +117,6 @@
             this.radioButton2.TabIndex = 1;
             this.radioButton2.Text = "Line";
             this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton1
             // 
@@ -142,7 +146,7 @@
             0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(62, 22);
-            this.numericUpDown1.TabIndex = 13;
+            this.numericUpDown1.TabIndex = 19;
             this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.numericUpDown1.Value = new decimal(new int[] {
             8,
@@ -156,7 +160,7 @@
             this.button3.Location = new System.Drawing.Point(449, 27);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 12;
+            this.button3.TabIndex = 18;
             this.button3.Text = "color";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -166,7 +170,7 @@
             this.button2.Location = new System.Drawing.Point(884, 24);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 11;
+            this.button2.TabIndex = 17;
             this.button2.Text = "Clear";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -176,24 +180,28 @@
             this.button1.Location = new System.Drawing.Point(624, 25);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 10;
+            this.button1.TabIndex = 16;
             this.button1.Text = "save";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // richTextBox1
+            // panel1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(767, 53);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(192, 504);
-            this.richTextBox1.TabIndex = 15;
-            this.richTextBox1.Text = "";
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(12, 75);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(749, 482);
+            this.panel1.TabIndex = 22;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(971, 569);
+            this.ClientSize = new System.Drawing.Size(972, 568);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.numericUpDown1);
@@ -204,10 +212,6 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
-            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
-            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
@@ -217,6 +221,7 @@
 
         #endregion
 
+        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton radioButton6;
         private System.Windows.Forms.RadioButton radioButton5;
@@ -228,7 +233,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
---
+++ _2.vcs/my_vcs_lesson_6_draw/_DrawTest_all/小畫家大全2/小畫家大全2/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Drawing.Imaging;   //for ImageFormat
+
+namespace 小畫家大全2
+{
+    public partial class Form1 : Form
+    {
+        private Point startPoint, oldPoint; //繪圖時紀錄滑鼠位置
+        //枚举类型，各种绘图工具
+        private enum drawTools
+        {
+            Pen = 0, Line, Circle, Rectangle, String, Erase, None
+        };
+        //当前使用的工具
+        private drawTools drawTool = drawTools.Pen;
+        private bool isDrawing = false;     //是否正在繪圖
+
+        Graphics g;         //Graphics實例
+        Pen PenStyle;
+        //SolidBrush sb;
+        Bitmap bitmap1;
+
+        Graphics ig;        //繪製bitmap的Graphics實例
+
+        Color foreColor = Color.Red;
+        Color backColor = Color.White;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //創建一個bitmap
+            bitmap1 = new Bitmap(this.ClientRectangle.Width, this.ClientRectangle.Height);
+            g = this.CreateGraphics();
+            ig = Graphics.FromImage(bitmap1);
+            //ig.Clear(backColor);
+
+            PenStyle = new Pen(foreColor);
+            PenStyle.Width = (int)numericUpDown1.Value;
+            PenStyle.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            PenStyle.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            PenStyle.Color = foreColor;
+
+            //PenStyle.LineJoin = System.Drawing.Drawing2D.LineJoin.Bevel;
+            PenStyle.LineJoin = System.Drawing.Drawing2D.LineJoin.Round;
+
+            g.Clear(backColor);
+            ig.Clear(backColor);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
