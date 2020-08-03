bdc4f7b14606ec8139fef3b4102a7f2c394c7c7a
david <david@insighteyes.com>
2020-08-03 14:07:16 +0800

    david add ox-game sample code.
    @sugar

---
+++ _2.vcs/my_vcs_lesson_5/vcs_OXGame/vcs_OXGame/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace vcs_OXGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(546, 464);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(552, 164);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(371, 300);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(552, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 40);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "show steps";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(552, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 40);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "reset steps";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(841, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 32);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 473);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ _2.vcs/my_vcs_lesson_5/vcs_OXGame/vcs_OXGame/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace vcs_OXGame
+{
+    public partial class Form1 : Form
+    {
+        //一維List for int
+        List<int> steps = new List<int>();
+        List<int> select_steps = new List<int>();
+        //int N;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            reset_steps();
+        }
+
+        void reset_steps()
+        {
+            int i;
+            int tmp;
+            Random r = new Random();
+
+            steps.Clear();
+            select_steps.Clear();
+
+            for (i = 0; i < 9; i++)
+            {
+                steps.Add(i + 1);
+                //steps.Add(new string[] { i.ToString(), ('A' + i).ToString() });
+            }
+
+            //show_steps(steps);
+
+            for (i = 0; i < steps.Count; i++)
+            {
+                int n = r.Next(steps.Count);
+                //richTextBox1.Text += "第" + i.ToString() + "項和第" + n.ToString() + "項交換\n";
+                tmp = steps[i];
+                steps[i] = steps[n];
+                steps[n] = tmp;
+            }
+            show_steps(steps);
+        }
+
+        void show_steps(List<int> steps)
+        {
+            int i;
+
+            if (steps.Count > 0)
+            {
+                richTextBox1.Text += "剩餘步數 " + steps.Count.ToString() + " 步, 分別是:\t";
+                for (i = 0; i < steps.Count; i++)
+                {
+                    richTextBox1.Text += steps[i].ToString() + "  ";
+                }
+                richTextBox1.Text += "\n";
+            }
+
+            if (select_steps.Count > 0)
+            {
+                richTextBox1.Text += "已走步數 " + select_steps.Count.ToString() + " 步, 分別是:\t";
+                for (i = 0; i < select_steps.Count; i++)
+                {
+                    richTextBox1.Text += select_steps[i].ToString() + "  ";
+                }
+                richTextBox1.Text += "\n";
+            }
+        }
+
+        void remove_item(int N)
+        {
+            int i;
+            if (steps.Count > 0)
+            {
+                for (i = 0; i < steps.Count; i++)
+                {
+                    if (N == steps[i])
+                    {
+                        richTextBox1.Text += "取得在index = " + i.ToString() + ",\t移除之\n";
+                        steps.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+
+            //revert color on N
+
+        }
+
+        void check_select_steps()
+        {
+            /*
+            int i;
+
+            if (select_steps.Count > 0)
+            {
+                richTextBox1.Text += "已走步數 " + select_steps.Count.ToString() + " 步, 分別是:\t";
+                for (i = 0; i < select_steps.Count; i++)
+                {
+                    richTextBox1.Text += select_steps[i].ToString() + "  ";
+                }
+                richTextBox1.Text += "\n";
+            }
+            */
+            if ((select_steps.Contains(1) == true) && (select_steps.Contains(2) == true) && (select_steps.Contains(3) == true))
+            {
+                richTextBox1.Text += "row 1 OK\n";
+            }
+            else if ((select_steps.Contains(4) == true) && (select_steps.Contains(5) == true) && (select_steps.Contains(6) == true))
+            {
+                richTextBox1.Text += "row 2 OK\n";
+            }
+            else if ((select_steps.Contains(7) == true) && (select_steps.Contains(8) == true) && (select_steps.Contains(9) == true))
+            {
+                richTextBox1.Text += "row 3 OK\n";
+            }
+            else if ((select_steps.Contains(1) == true) && (select_steps.Contains(4) == true) && (select_steps.Contains(7) == true))
+            {
+                richTextBox1.Text += "column 1 OK\n";
+            }
+            else if ((select_steps.Contains(2) == true) && (select_steps.Contains(5) == true) && (select_steps.Contains(8) == true))
+            {
+                richTextBox1.Text += "column 2 OK\n";
+            }
+            else if ((select_steps.Contains(3) == true) && (select_steps.Contains(6) == true) && (select_steps.Contains(9) == true))
+            {
+                richTextBox1.Text += "column 3 OK\n";
+            }
+            else if ((select_steps.Contains(1) == true) && (select_steps.Contains(5) == true) && (select_steps.Contains(9) == true))
+            {
+                richTextBox1.Text += "cross 1 OK\n";
+            }
+            else if ((select_steps.Contains(3) == true) && (select_steps.Contains(5) == true) && (select_steps.Contains(7) == true))
+            {
+                richTextBox1.Text += "cross 2 OK\n";
+            }
+
+
+
+        }
+
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.Text = "目前滑鼠在Form1上的位置：" + e.X + " : " + e.Y;
+            int N = 0;
+            int W = pictureBox1.Width;
+            int H = pictureBox1.Height;
+            int x = e.X;
+            int y = e.Y;
+            int w = W / 3;
+            int h = H / 3;
+            int dd = 10;
+
+            if ((x < dd) || (y < dd) || (x >= (W - dd)) || (y >= (H - dd)))
+                return;
+
+            if (y < (h - dd))
+            {
+                if (x < (w - dd))
+                {
+                    richTextBox1.Text += "左上\n";
+                    N = 1;
+                }
+                else if (x < (w * 2 - dd))
+                {
+                    richTextBox1.Text += "中上\n";
+                    N = 2;
+                }
+                else
+                {
+                    richTextBox1.Text += "右上\n";
+                    N = 3;
+                }
+            }
+            else if (y < (h * 2 - dd))
+            {
+                if (x < (w - dd))
+                {
+                    richTextBox1.Text += "左中\n";
+                    N = 4;
+                }
+                else if (x < (w * 2 - dd))
+                {
+                    richTextBox1.Text += "中中\n";
+                    N = 5;
+                }
+                else
+                {
+                    richTextBox1.Text += "右中\n";
+                    N = 6;
+                }
+            }
+            else
+            {
+                if (x < (w - dd))
+                {
+                    richTextBox1.Text += "左下\n";
+                    N = 7;
+                }
+                else if (x < (w * 2 - dd))
+                {
+                    richTextBox1.Text += "中下\n";
+                    N = 8;
+                }
+                else
+                {
+                    richTextBox1.Text += "右下\n";
+                    N = 9;
+                }
+            }
+
+            richTextBox1.Text += "取得 N = " + N.ToString() + "\n";
+
+            remove_item(N);
+            select_steps.Add(N);
+            check_select_steps();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            reset_steps();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            show_steps(steps);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+    }
+}
