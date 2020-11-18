e88d3d500249398fb828f28424d80c7dd5ea5efd
ims_david <mysonbldc@gmail.com>
2020-11-19 00:06:50 +0800

    david modify webcam code
    rename
    
    @julia

---
+++ _2.vcs/my_vcs_lesson_6/_WebCam/__非AForge/vcs_WebCam_Emgu2/vcs_WebCam_Emgu2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace vcs_WebCam_Emgu2
+{
+    public partial class Form1 : Form
+    {
+        private Capture cap1 = null;
+        private Capture cap2 = null;
+        private Capture cap3 = null;
+        private Capture cap4 = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            Image<Bgr, Byte> frame1 = cap1.QueryFrame(); // 去query該畫面
+            Image<Bgr, Byte> frame2 = cap2.QueryFrame(); // 去query該畫面
+            Image<Bgr, Byte> frame3 = cap3.QueryFrame(); // 去query該畫面
+            Image<Bgr, Byte> frame4 = cap4.QueryFrame(); // 去query該畫面
+
+            pictureBox1.Image = frame1.ToBitmap(); // 把畫面轉換成bitmap型態，在餵給pictureBox元件
+            pictureBox2.Image = frame2.ToBitmap(); // 把畫面轉換成bitmap型態，在餵給pictureBox元件
+            pictureBox3.Image = frame3.ToBitmap(); // 把畫面轉換成bitmap型態，在餵給pictureBox元件
+            pictureBox4.Image = frame4.ToBitmap(); // 把畫面轉換成bitmap型態，在餵給pictureBox元件
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cap1 = new Capture(0);
+            cap2 = new Capture(0);
+            cap3 = new Capture(0);
+            cap4 = new Capture(0);
+            cap2.FlipHorizontal = true;
+            cap3.FlipVertical = true;
+            cap4.FlipHorizontal = true;
+            cap4.FlipVertical = true;
+
+            richTextBox1.Text += "width = " + cap1.Width.ToString() + "\n";
+            richTextBox1.Text += "height = " + cap1.Height.ToString() + "\n";
+            Application.Idle += new EventHandler(Application_Idle);
+        }
+
+    }
+}
