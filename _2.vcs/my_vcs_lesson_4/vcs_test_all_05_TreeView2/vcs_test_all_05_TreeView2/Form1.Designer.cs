f936b0e091085640f254db69ff9fa4bee982e783
ims_david <mysonbldc@gmail.com>
2020-11-20 01:14:26 +0800

    david modify vcs code.
    @julia

---
+++ _2.vcs/my_vcs_lesson_4/vcs_test_all_05_TreeView2/vcs_test_all_05_TreeView2/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace vcs_test_all_05_TreeView2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(494, 399);
+            this.treeView1.TabIndex = 0;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Folder_Open.png");
+            this.imageList1.Images.SetKeyName(1, "Document_Text.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 414);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ _2.vcs/my_vcs_lesson_4/vcs_test_all_05_TreeView2/vcs_test_all_05_TreeView2/TreeViewExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+using System.IO;
+
+namespace vcs_test_all_05_TreeView2
+{
+    public static class TreeViewExtensions
+    {
+        // Initialize the TreeView from a directory,
+        // its subdirectories, and their files.
+        public static void LoadFromDirectory(this TreeView trv, string directory, int folder_img, int file_img)
+        {
+            DirectoryInfo dir_info = new DirectoryInfo(directory);
+            AddDirectoryNodes(trv, dir_info, null, folder_img, file_img);
+        }
+
+        // Add this directory's node and sub-nodes.
+        public static void AddDirectoryNodes(TreeView trv, DirectoryInfo dir_info, TreeNode parent, int folder_img, int file_img)
+        {
+            // Add the directory's node.
+            TreeNode dir_node;
+            if (parent == null) dir_node = trv.Nodes.Add(dir_info.Name);
+            else dir_node = parent.Nodes.Add(dir_info.Name);
+
+            // Add the folder image.
+            if (folder_img >= 0) dir_node.ImageIndex = folder_img;
+
+            // Add subdirectories.
+            foreach (DirectoryInfo subdir in dir_info.GetDirectories())
+                AddDirectoryNodes(trv, subdir, dir_node, folder_img, file_img);
+
+            // Add file nodes.
+            foreach (FileInfo file_info in dir_info.GetFiles())
+            {
+                TreeNode file_node = dir_node.Nodes.Add(file_info.Name);
+                if (file_img >= 0) file_node.ImageIndex = file_img;
+            }
+        }
+    }
+}
