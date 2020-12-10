e336f6ae42e13c00b888c4734905159ef7a97bc5
david <bunshue@gmail.com>
2020-12-11 00:32:50 +0800

    david modify vcs code
    @romeo

---
+++ _2.vcs/my_vcs_lesson_6_draw/vcs_DrawD_Map/vcs_DrawD_Map/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace vcs_DrawD_Map
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(476, 323);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1037, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "標出景點";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1037, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1180, 640);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ _2.vcs/my_vcs_lesson_6_draw/vcs_DrawD_Map/vcs_DrawD_Map/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace vcs_DrawD_Map
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen p;
+        SolidBrush sb;
+        Bitmap bitmap1;
+
+        // The hotspots.
+        private List<Rectangle> Hotspots = new List<Rectangle>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            p = new Pen(Color.Red, 3);
+
+            pictureBox1.Image = vcs_DrawD_Map.Properties.Resources.大峽谷國家公園;
+            pictureBox1.ClientSize = new Size(pictureBox1.Image.Size.Width, pictureBox1.Image.Size.Height);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            this.Size = new Size(pictureBox1.Image.Size.Width + 200, pictureBox1.Image.Size.Height + 50);
+            button1.Location = new Point(pictureBox1.Image.Width + 20, 10);
+            richTextBox1.Location = new Point(pictureBox1.Image.Width + 20, 40);
+            richTextBox1.Size = new Size(150, pictureBox1.Size.Height - 40);
+
+            // The hotspots.
+            // Initialize the hotspots.
+            Hotspots.Add(new Rectangle(88, 509, 22, 22));
+            Hotspots.Add(new Rectangle(140, 577, 20, 20));
+            Hotspots.Add(new Rectangle(161, 609, 20, 20));
+            Hotspots.Add(new Rectangle(630, 138, 20, 20));
+            Hotspots.Add(new Rectangle(447, 626, 20, 20));
+            Hotspots.Add(new Rectangle(966, 179, 20, 20));
+            Hotspots.Add(new Rectangle(958, 214, 20, 20));
+            Hotspots.Add(new Rectangle(1062, 301, 20, 20));
+            Hotspots.Add(new Rectangle(1109, 581, 20, 20));
+            Hotspots.Add(new Rectangle(1099, 621, 20, 20));
+            Hotspots.Add(new Rectangle(1247, 262, 20, 16));
+            Hotspots.Add(new Rectangle(1314, 224, 20, 20));
+            Hotspots.Add(new Rectangle(1344, 651, 20, 20));
+            Hotspots.Add(new Rectangle(1098, 753, 20, 20));
+            Hotspots.Add(new Rectangle(655, 797, 20, 20));
+            Hotspots.Add(new Rectangle(549, 846, 20, 20));
+            Hotspots.Add(new Rectangle(449, 935, 20, 20));
+            Hotspots.Add(new Rectangle(826, 876, 20, 20));
+            Hotspots.Add(new Rectangle(991, 930, 20, 20));
+            Hotspots.Add(new Rectangle(1095, 900, 20, 20));
+            Hotspots.Add(new Rectangle(1249, 942, 20, 20));
+            Hotspots.Add(new Rectangle(254, 1079, 20, 20));
+            Hotspots.Add(new Rectangle(298, 1110, 20, 20));
+            Hotspots.Add(new Rectangle(1234, 1076, 16, 18));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+
+
+            int len = Hotspots.Count;
+            //richTextBox1.Text += "len = " + len.ToString() + "\n";
+            /*  same
+            int i;
+            for (i = 0; i < len; i++)
+            {
+                g.DrawRectangle(p, Hotspots[i]);
+            }
+            */
+            //same
+            foreach (Rectangle hotspot in Hotspots)
+            {
+                g.FillRectangle(Brushes.Red, hotspot);
+            }
+        }
+
+        // See if we clicked a hotspot.
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int i = HotspotAtPoint(e.Location);
+            if (i >= 0)
+                richTextBox1.Text += "你點中景點 " + i.ToString() + "\n";
+        }
+
+        // See if we're over a hotspot.
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // See if we're over a hotspot.
+            int i = HotspotAtPoint(e.Location);
+            if (i >= 0)
+            {
+                //richTextBox1.Text += "你經過景點 " + i.ToString() + "\n";
+                pictureBox1.Cursor = Cursors.Hand;
+            }
+            else
+                pictureBox1.Cursor = Cursors.Default;
+        }
+
+        // Return the index of the hotspot at this point
+        // or -1 if there is no hotspot there.
+        private int HotspotAtPoint(Point mouse_point)
+        {
+            //richTextBox1.Text += "(" + mouse_point.X.ToString() + ", " + mouse_point.Y.ToString() + ") ";
+
+            // Check the hotspots.
+            //return Hotspots.FindIndex(hotspot => hotspot.Contains(mouse_point));
+
+            for (int i = 0; i < Hotspots.Count; i++)
+                if (Hotspots[i].Contains(mouse_point)) return i;
+
+            // We didn't find a hotspot that contains the point.
+            return -1;
+        }
+
+
+
+    }
+}
