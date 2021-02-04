113f5116d93290e9d8670d03bb5c2f0eb7cb259a
david <bunshue@gmail.com>
2021-02-05 01:02:01 +0800

    david modify vcs  code
    @romeo

---
+++ _2.vcs/my_vcs_lesson_6_picture_image/vcs_ImageProcessing6/vcs_ImageProcessing6/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace vcs_ImageProcessing6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picHidden = new System.Windows.Forms.PictureBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnLockBits = new System.Windows.Forms.Button();
+            this.btnNoLockBits = new System.Windows.Forms.Button();
+            this.picVisible = new System.Windows.Forms.PictureBox();
+            this.lblElapsed = new System.Windows.Forms.Label();
+            this.btnQuarter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picHidden)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVisible)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picHidden
+            // 
+            this.picHidden.Image = ((System.Drawing.Image)(resources.GetObject("picHidden.Image")));
+            this.picHidden.Location = new System.Drawing.Point(184, 104);
+            this.picHidden.Name = "picHidden";
+            this.picHidden.Size = new System.Drawing.Size(305, 400);
+            this.picHidden.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picHidden.TabIndex = 1;
+            this.picHidden.TabStop = false;
+            this.picHidden.Visible = false;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(12, 11);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(80, 22);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnLockBits
+            // 
+            this.btnLockBits.Location = new System.Drawing.Point(184, 11);
+            this.btnLockBits.Name = "btnLockBits";
+            this.btnLockBits.Size = new System.Drawing.Size(80, 22);
+            this.btnLockBits.TabIndex = 5;
+            this.btnLockBits.Text = "Lock Bits";
+            this.btnLockBits.Click += new System.EventHandler(this.btnLockBits_Click);
+            // 
+            // btnNoLockBits
+            // 
+            this.btnNoLockBits.Location = new System.Drawing.Point(98, 11);
+            this.btnNoLockBits.Name = "btnNoLockBits";
+            this.btnNoLockBits.Size = new System.Drawing.Size(80, 22);
+            this.btnNoLockBits.TabIndex = 4;
+            this.btnNoLockBits.Text = "No Lock Bits";
+            this.btnNoLockBits.Click += new System.EventHandler(this.btnNoLockBits_Click);
+            // 
+            // picVisible
+            // 
+            this.picVisible.Location = new System.Drawing.Point(12, 39);
+            this.picVisible.Name = "picVisible";
+            this.picVisible.Size = new System.Drawing.Size(446, 577);
+            this.picVisible.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picVisible.TabIndex = 6;
+            this.picVisible.TabStop = false;
+            // 
+            // lblElapsed
+            // 
+            this.lblElapsed.AutoSize = true;
+            this.lblElapsed.Location = new System.Drawing.Point(12, 574);
+            this.lblElapsed.Name = "lblElapsed";
+            this.lblElapsed.Size = new System.Drawing.Size(0, 12);
+            this.lblElapsed.TabIndex = 7;
+            // 
+            // btnQuarter
+            // 
+            this.btnQuarter.Location = new System.Drawing.Point(270, 11);
+            this.btnQuarter.Name = "btnQuarter";
+            this.btnQuarter.Size = new System.Drawing.Size(80, 22);
+            this.btnQuarter.TabIndex = 8;
+            this.btnQuarter.Text = "Quarter";
+            this.btnQuarter.Click += new System.EventHandler(this.btnQuarter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 597);
+            this.Controls.Add(this.btnQuarter);
+            this.Controls.Add(this.lblElapsed);
+            this.Controls.Add(this.picVisible);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnLockBits);
+            this.Controls.Add(this.btnNoLockBits);
+            this.Controls.Add(this.picHidden);
+            this.Name = "Form1";
+            this.Text = "vcs_ImageProcessing6";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picHidden)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVisible)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.PictureBox picHidden;
+        internal System.Windows.Forms.Button btnReset;
+        internal System.Windows.Forms.Button btnLockBits;
+        internal System.Windows.Forms.Button btnNoLockBits;
+        internal System.Windows.Forms.PictureBox picVisible;
+        private System.Windows.Forms.Label lblElapsed;
+        internal System.Windows.Forms.Button btnQuarter;
+    }
+}
+
---
+++ _2.vcs/my_vcs_lesson_6_picture_image/vcs_ImageProcessing6/vcs_ImageProcessing6/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace vcs_ImageProcessing6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Display the initial image.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            picVisible.Image = picHidden.Image.Clone() as Image;
+        }
+
+        // Display the original image.
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            picVisible.Image = picHidden.Image.Clone() as Image;
+            lblElapsed.Text = "";
+        }
+
+        private const int NUM_TRIALS = 5;
+
+        // Invert the image without Lockbits.
+        private void btnNoLockBits_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            Bitmap bm = new Bitmap(picHidden.Image);
+            for (int trial = 0; trial < NUM_TRIALS; trial++)
+            {
+                for (int Y = 0; Y < bm.Height; Y++)
+                {
+                    for (int X = 0; X < bm.Width; X++)
+                    {
+                        Color clr = bm.GetPixel(X, Y);
+                        clr = Color.FromArgb(
+                            255 - clr.R,
+                            255 - clr.G,
+                            255 - clr.B);
+                        bm.SetPixel(X, Y, clr);
+                    }
+                }
+            }
+            picVisible.Image = bm;
+            watch.Stop();
+            Cursor = Cursors.Default;
+            lblElapsed.Text = watch.Elapsed.TotalSeconds.ToString("0.000000") + " seconds";
+        }
+
+        // Invert the image using Lockbits.
+        private void btnLockBits_Click(object sender, EventArgs e)
+        {
+            const byte BYTE_255 = 255;
+            Bitmap bm = new Bitmap(picHidden.Image);
+            Cursor = Cursors.WaitCursor;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            
+            for (int trial = 0; trial < NUM_TRIALS; trial++)
+            {
+                // Make a Bitmap24 object.
+                Bitmap24 bm24 = new Bitmap24(bm);
+
+                // Lock the bitmap.
+                bm24.LockBitmap();
+
+                // Invert the pixels.
+                byte red, green, blue;
+                for (int y = 0; y < bm24.Height; y++)
+                {
+                    for (int x = 0; x < bm24.Width; x++)
+                    {
+                        bm24.GetPixel(x, y, out red, out green, out blue);
+                        red = (byte)(BYTE_255 - red);
+                        green = (byte)(BYTE_255 - green);
+                        blue = (byte)(BYTE_255 - blue);
+                        bm24.SetPixel(x, y, red, green, blue);
+                    }
+                }
+
+                /*
+                //same but slower
+                // Invert the pixels.
+                for (int i = 0; i < bm.Height * bm24.RowSizeBytes; i++)
+                {
+                    // bm24.ImageBytes[i] = Convert.ToByte(BYTE_255 - bm24.ImageBytes[i]);
+                    bm24.ImageBytes[i] = (byte)(BYTE_255 - bm24.ImageBytes[i]);
+                }
+                */
+
+                // Unlock the bitmap.
+                bm24.UnlockBitmap();
+            }
+            picVisible.Image = bm;
+
+            watch.Stop();
+            Cursor = Cursors.Default;
+            lblElapsed.Text = watch.Elapsed.TotalSeconds.ToString("0.000000") + " seconds";
+        }
+
+        private void btnQuarter_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(picHidden.Image);
+            Cursor = Cursors.WaitCursor;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            // Make a Bitmap24 object.
+            Bitmap24 bm24 = new Bitmap24(bm);
+
+            // Lock the bitmap.
+            bm24.LockBitmap();
+
+            // Invert the pixels.
+            int xmid = bm24.Width / 2;
+            int ymid = bm24.Height / 2;
+            for (int y = 0; y < ymid; y++)
+            {
+                for (int x = 0; x < xmid; x++)
+                {
+                    bm24.SetGreen(x, y, 0);
+                    bm24.SetBlue(x, y, 0);
+                }
+            }
+            for (int y = ymid; y < bm24.Height; y++)
+            {
+                for (int x = 0; x < xmid; x++)
+                {
+                    bm24.SetRed(x, y, 0);
+                    bm24.SetGreen(x, y, 0);
+                }
+            }
+            for (int y = 0; y < ymid; y++)
+            {
+                for (int x = xmid; x < bm24.Width; x++)
+                {
+                    bm24.SetRed(x, y, 0);
+                    bm24.SetBlue(x, y, 0);
+                }
+            }
+            byte red, green, blue;
+            for (int y = ymid; y < bm24.Height; y++)
+            {
+                for (int x = xmid; x < bm24.Width; x++)
+                {
+                    red = (byte)(255 - bm24.GetRed(x, y));
+                    green = (byte)(255 - bm24.GetGreen(x, y));
+                    blue = (byte)(255 - bm24.GetBlue(x, y));
+                    bm24.SetPixel(x, y, red, green, blue);
+                }
+            }
+
+            // Unlock the bitmap.
+            bm24.UnlockBitmap();
+            picVisible.Image = bm;
+
+            watch.Stop();
+            Cursor = Cursors.Default;
+            lblElapsed.Text =
+                watch.Elapsed.TotalSeconds.ToString("0.000000") +
+                " seconds";
+        }
+    }
+}
