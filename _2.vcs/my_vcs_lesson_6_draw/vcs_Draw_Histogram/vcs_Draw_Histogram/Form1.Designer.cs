ce0db8d5caa0c381c01e90a7dd3222c419c75f1a
david <bunshue@gmail.com>
2021-02-23 00:49:33 +0800

    david modify vcs code
    @ romeo

--- _2.vcs/my_vcs_lesson_6_draw/vcs_Draw_Histogram/vcs_Draw_Histogram/Form1.Designer.cs
+++ _2.vcs/my_vcs_lesson_6_draw/vcs_Draw_Histogram/vcs_Draw_Histogram/Form1.Designer.cs
@@ -29,12 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button2 = new System.Windows.Forms.Button();
             this.bt_clear = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.pictureBox_histogram = new System.Windows.Forms.PictureBox();
@@ -45,16 +42,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_histogram)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_old)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(760, 27);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
@@ -74,16 +61,6 @@
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(760, 74);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // bt_clear
             // 
             this.bt_clear.Location = new System.Drawing.Point(637, 379);
@@ -94,21 +71,11 @@
             this.bt_clear.UseVisualStyleBackColor = true;
             this.bt_clear.Click += new System.EventHandler(this.bt_clear_Click);
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(760, 127);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(760, 173);
+            this.button4.Location = new System.Drawing.Point(760, 25);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(90, 35);
             this.button4.TabIndex = 6;
             this.button4.Text = "my";
             this.button4.UseVisualStyleBackColor = true;
@@ -116,9 +83,9 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(760, 219);
+            this.button5.Location = new System.Drawing.Point(762, 83);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(90, 35);
             this.button5.TabIndex = 7;
             this.button5.Text = "my int";
             this.button5.UseVisualStyleBackColor = true;
@@ -142,9 +109,9 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(760, 263);
+            this.button6.Location = new System.Drawing.Point(762, 140);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.Size = new System.Drawing.Size(90, 35);
             this.button6.TabIndex = 9;
             this.button6.Text = "my float";
             this.button6.UseVisualStyleBackColor = true;
@@ -170,12 +137,9 @@
             this.Controls.Add(this.pictureBox_histogram);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.bt_clear);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -188,12 +152,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button bt_clear;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.PictureBox pictureBox_histogram;
