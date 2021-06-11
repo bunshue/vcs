91128891d60d1cb8d3dd1042ba3cbb9c47bfacf5
david <david@insighteyes.com>
2021-06-11 19:39:51 +0800

    david modify vcs code

---
+++ _2.vcs/my_vcs_lesson_c_example/_bookbook/VC#2008網路遊戲程式設計/vcs_Network4/_client/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets; //ServerSocket = new Socket(...)時使用
+using System.Threading;  //Thread時使用
+using System.Text;       //Encoding.Unicode.GetString(...) 時使用
+using System.IO;         //使用FileInfo類別，來建立一個檔案實體物件
+
+namespace _client
+{
+   
+    public partial class Form1 : Form
+    {
+        private Thread _thread1;
+        private Socket ClientSocket;
+        Form2 f2 = new Form2();
+        delegate void mydel(string str);
+        int Isconn = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        static string namescore;
+        public string NAMESCORE
+        {
+
+            set { namescore = value; }
+
+
+            get { return namescore; }
+
+        }
+
+        /*
+        protected override void Dispose(bool disposing)
+        {
+            //清除資源
+
+
+            if (Isconn == 1)
+            {
+                byte[] byteReceieve = new byte[1024];
+                byte[] byteSend = new byte[1024];
+                string disconnect = "disconnect";
+                Isconn = 0;
+                byteSend = Encoding.Unicode.GetBytes(disconnect.ToCharArray());
+                ClientSocket.Send(byteSend, 0, byteSend.Length, SocketFlags.None);
+                Thread.Sleep(50);
+                byteSend = Encoding.Unicode.GetBytes(textBox2.Text.ToCharArray());//若斷線，則傳給server斷線的字串
+                ClientSocket.Send(byteSend, 0, byteSend.Length, SocketFlags.None);
+
+                ClientSocket.Shutdown(SocketShutdown.Both);
+                ClientSocket.Close();
+                _thread1.Abort();
+
+                Application.Exit();
+            }
+            else Application.Exit();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+         */
+        private void Show_listBox_1(string str)//此副程式為了避免跨執行緒錯誤
+        {
+            mydel a = Show_listBox_1;
+            object[] obj;
+            obj = new object[1];
+            obj[0] = str;
+            if ((listBox1.InvokeRequired)) this.Invoke(a, obj);
+            else
+            {
+
+                this.listBox1.Items.Add((string)obj[0]);
+
+            }
+
+        }
+        private void Show_listBox_2(string str)//此副程式為了避免跨執行緒錯誤
+        {
+            mydel a = Show_listBox_2;
+            object[] obj;
+            obj = new object[1];
+            obj[0] = str;
+            if ((listBox1.InvokeRequired)) this.Invoke(a, obj);
+            else
+            {
+                this.listBox1.Items.Remove((string)obj[0]);
+            }
+
+        }
+        private void Show_label4(string str)//此副程式為了避免跨執行緒錯誤
+        {
+            mydel a = Show_label4;
+            object[] obj;
+            obj = new object[1];
+            obj[0] = str;
+            if (label4.InvokeRequired) this.Invoke(a, obj);
+            else this.label4.Text = (string)obj[0];
+        }
+        private void Show_listBox_3()//此副程式為了避免跨執行緒錯誤
+        {
+            if (listBox1.InvokeRequired) listBox1.Invoke(new MethodInvoker(Show_listBox_3));
+            else
+            {
+
+                this.listBox1.Items.Clear();
+            }
+        }
+        
+        private void Listen()
+        {
+            byte[] byteReceieve = new byte[1024];
+            byte[] byteSend = new byte[1024];
+
+            EndPoint oldEP = ClientSocket.RemoteEndPoint;
+            Thread tempthread = _thread1;
+
+            int ii, six = 0, c_conn = 0, disconnect = 0, Disconnect = 0, Show = 0, update = 0;
+            string strAll;
+            while (true)
+            {
+
+                try
+                {
+
+                    ii = ClientSocket.ReceiveFrom(byteReceieve, 0, byteReceieve.Length, SocketFlags.None, ref oldEP);
+
+
+                    strAll = Encoding.Unicode.GetString(byteReceieve, 0, ii);
+
+                    if (strAll == "c_conn") c_conn = 1;
+                    else if (strAll == "Show") Show = 1;
+                    else if (strAll == "disconnect") disconnect = 1;
+                    else if (strAll == "Disconnect") Disconnect = 1;
+                    else if (strAll == "six") six = 1;
+                    if ((c_conn == 1) && (strAll != "") && (strAll != "c_conn") && (strAll != "Show") &&
+                        (strAll != "Disconnect") && (strAll != "disconnect") && (strAll != "score"))
+                    {
+                        c_conn = 0;
+                        Show_listBox_3();
+                        Show_label4(strAll);
+
+                    }
+
+                    if ((Show == 1) && (strAll != "") && (strAll != "Show") && (strAll != "disconnect") &&
+                        (strAll != "Disconnect") && (strAll != "c_conn") && (strAll != "score"))
+                    {
+                        Show = 0;
+                        Show_listBox_1(strAll);
+
+                    }
+
+                    if ((disconnect == 1) && (strAll != "") && (strAll != "disconnect") && (strAll != "Disconnect") &&
+                        (strAll != "Show") && (strAll != "c_conn") && (strAll != "score"))
+                    {
+                        disconnect = 0;
+                        Show_label4(strAll);
+
+                    }
+
+                    if ((Disconnect == 1) && (strAll != "") && (strAll != "Disconnect") && (strAll != "score") &&
+                        (strAll != "disconnect") && (strAll != "Show") && (strAll != "c_conn")  )
+                    {
+                        Disconnect = 0;
+                        Show_listBox_2(strAll);
+
+                    }
+                     if ((six == 1) && (strAll != "") && (strAll != "disconnect") && (strAll != "Disconnect") &&
+                        (strAll != "Show") && (strAll != "c_conn") && (strAll != "six"))
+                    {
+                        six = 0;
+                        f2.NAMESCORE = strAll;
+                    }
+                }
+                catch
+                {
+                    ;
+                }
+
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] byteReceieve = new byte[1024];
+            byte[] byteSend = new byte[1024];
+
+            Form2 f2 = new Form2();
+            f2.SNAME = textBox2.Text;
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show("請輸入姓名", "玩家");
+            }
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("請輸入IP位址", "玩家");
+            }
+            else
+            {
+                try
+                {
+                    if (Isconn == 0)
+                    {
+                        ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        IPEndPoint hostEP = new IPEndPoint(IPAddress.Parse(textBox1.Text), 233);
+
+                        button1.Enabled = false;
+                        button3.Enabled = true;
+                        button2.Enabled = true;
+                        textBox1.ReadOnly = true;
+                        textBox2.ReadOnly = true;
+                        ClientSocket.Connect(hostEP);
+                        Isconn = 1;
+                        _thread1 = new Thread(new ThreadStart(Listen));//讓_thread1去執行Listen()副函式
+                        _thread1.Start();//啟動_thread1
+                        Thread.Sleep(200);
+                        byteSend = Encoding.Unicode.GetBytes("name".ToCharArray());
+                        ClientSocket.Send(byteSend, 0, byteSend.Length, SocketFlags.None);//傳送byteSend資料給Server
+                        Thread.Sleep(50);
+                        byteSend = Encoding.Unicode.GetBytes(textBox2.Text.ToCharArray());
+                        ClientSocket.Send(byteSend, 0, byteSend.Length, SocketFlags.None);//傳送byteSend資料給Server
+                        MessageBox.Show("連線成功！", "玩家");
+                        
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("IP錯誤", "玩家");
+
+                    button1.Enabled = true;
+                    button2.Enabled = false;
+                    textBox1.ReadOnly = false;
+                    textBox2.ReadOnly = false;
+
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] byteReceieve = new byte[1024];
+            byte[] byteSend = new byte[1024];
+            if (Isconn == 1)
+            {
+
+                textBox1.ReadOnly = false;
+                textBox2.ReadOnly = false;
+                button1.Enabled = true;
+                button2.Enabled = false;
+                byteSend = Encoding.Unicode.GetBytes("disconnect".ToCharArray());
+                ClientSocket.Send(byteSend, 0, byteSend.Length, SocketFlags.None);
+                Thread.Sleep(50);
+                byteSend = Encoding.Unicode.GetBytes(textBox2.Text.ToCharArray());//若斷線，則傳給server斷線的字串
+                ClientSocket.Send(byteSend, 0, byteSend.Length, SocketFlags.None);
+                Isconn = 0;
+                ClientSocket.Shutdown(SocketShutdown.Both);
+                ClientSocket.Close();
+                _thread1.Abort();
+                label4.Text = "";
+                listBox1.Items.Clear();
+                MessageBox.Show("已離線!", "玩家");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            timer1.Enabled = true;
+            f2.Show(this);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            byte[] byteReceieve = new byte[1024];
+            byte[] byteSend = new byte[1024];
+            
+                if (namescore != null)
+                {
+                    byteSend = Encoding.Unicode.GetBytes("six".ToCharArray());
+                    ClientSocket.Send(byteSend, 0, byteSend.Length, SocketFlags.None);
+                    Thread.Sleep(50);
+                    byteSend = Encoding.Unicode.GetBytes(namescore.ToCharArray());//若斷線，則傳給server斷線的字串
+                    ClientSocket.Send(byteSend, 0, byteSend.Length, SocketFlags.None);
+                    namescore = null;
+                }
+            
+        }
+    }
+}
---
+++ _2.vcs/my_vcs_lesson_c_example/_bookbook/VC#2008網路遊戲程式設計/vcs_Network4/_client/Form2.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _client
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+        }
+        int cnt_name = 0, right_num = 0, score = 0, x1 = 0, a = 0;
+
+        private Hashtable name_score = new Hashtable(); // 使用hashtable 來記錄暱稱與分數 的資訊
+        public static string namescore;
+        public string NAMESCORE
+        {
+
+            set { namescore = value; }
+
+
+            get { return namescore; }
+
+        }
+
+        static string Sname;
+        public string SNAME
+        {
+
+            set { Sname = value; }
+
+
+            get { return Sname; }
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            right_num += 1;
+            button3.Enabled = false;
+            label3.Visible = true;
+            label4.Visible = true;
+            timer1.Enabled = true;
+            timer2.Enabled = false;
+            a = 10;
+            label4.Text = "玩家" + Sname + " 的分數為 :" + score.ToString();
+            label3.Text = "你已答了" + right_num.ToString() + "題";
+            label6.Text = "放棄了...";
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            if (x1 == 1)
+            {
+                if (radioButton1.Checked == true)
+                {
+                    label6.Text = Sname + " 恭喜你答對了 ";
+                    score = score + 20;
+                    a = 10;
+                }
+                else
+                {
+                    label6.Text = Sname + " 你答錯了 ";
+                    score -= 5;
+                    a = 10;
+                }
+                right_num += 1;
+            }
+            else if (x1 == 2)
+            {
+                if (radioButton2.Checked == true)
+                {
+                    label6.Text = Sname + " 恭喜你答對了 ";
+                    score = score + 20;
+                    a = 10;
+                }
+                else
+                {
+                    label6.Text = Sname + " 你答錯了 ";
+                    score -= 5;
+                    a = 10;
+                }
+                right_num += 1;
+            }
+            else if (x1 == 3)
+            {
+                if (radioButton3.Checked == true)
+                {
+                    label6.Text = Sname + " 恭喜你答對了 ";
+                    score = score + 20;
+                    a = 10;
+                }
+                else
+                {
+                    label6.Text = Sname + " 你答錯了 ";
+                    score -= 5;
+                }
+                right_num += 1;
+            }
+            else if (x1 == 4)
+            {
+                if (radioButton4.Checked == true)
+                {
+                    label6.Text = Sname + " 恭喜你答對了 ";
+                    score = score + 20;
+                    a = 10;
+                }
+                else
+                {
+                    label6.Text = Sname + " 你答錯了 ";
+                    score -= 5;
+                    a = 10;
+                }
+                right_num += 1;
+            }
+            else if (x1 == 5)
+            {
+                if (radioButton4.Checked == true)
+                {
+                    label6.Text = Sname + " 恭喜你答對了 ";
+                    score = score + 20;
+                    a = 10;
+                }
+                else
+                {
+                    label6.Text = Sname + " 你答錯了 ";
+                    score -= 5;
+                    a = 10;
+                }
+                right_num += 1;
+            }
+            else if (x1 == 6)
+            {
+                if (radioButton4.Checked == true)
+                {
+                    label6.Text = Sname + " 恭喜你答對了 ";
+                    score = score + 20;
+                    a = 10;
+                }
+                else
+                {
+                    label6.Text = Sname + " 你答錯了 ";
+                    score -= 5;
+                    a = 10;
+                }
+                right_num += 1;
+            }
+            button2.Enabled = false;
+            label3.Visible = true;
+            label4.Visible = true;
+            label6.Visible = true;
+
+            timer1.Enabled = true; //出題目
+            timer2.Enabled = false;//秒數
+
+            label4.Text = "玩家" + Sname + " 的分數為 :" + score.ToString();
+            label3.Text = "你已答了" + right_num.ToString() + "題";
+
+          
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            int[] avg = new int[5];
+            string[] name = new string[5];
+            string[] str = new string[10];
+            string tempAVG;
+
+            int i = 0, num = 0;
+            int[] found = new int[5];
+            foreach (DictionaryEntry de in name_score)
+            {
+                str[num] = (string)de.Key;
+                ++num;
+            }
+
+
+            foreach (string t in str) //分解字串存入陣列
+            {
+                if (str[i] != null)
+                {
+
+                    found[i] = t.IndexOf("|");
+                    name[i] = t.Substring(0, (found[i]));
+                    tempAVG = t.Substring(found[i] + 1);
+                    avg[i] = int.Parse(tempAVG);
+                    ++i;
+                }
+
+            }
+
+            Array.Sort(avg, name);
+
+            for (i = 0; i < num; ++i)
+            {
+                label18.Text = label18.Text + "第" + (i + 1) + "名" + name[4 - i] + "   " + avg[4 - i].ToString() + "分\n";
+            }
+           
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label7.Text = a.ToString();
+            a -= 1;
+
+            if (label7.Text != "0")
+            {
+                timer1.Enabled = false;//出題目
+                timer2.Enabled = true; //秒數
+            }
+            else
+            {
+                timer1.Enabled = true;//出題目
+                timer2.Enabled = false;//秒數
+                a = 10;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+           // label2.Text = "玩家" + Sname;
+            label3.Text = " 你已答了" + right_num + " 題";
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+
+            if (cnt_name == 1)
+            {
+                label8.Text = "參賽者 :" + Sname;
+                label13.Text = score.ToString() + "分";
+
+            }
+
+
+           // label8.Text = label8.Text;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            button2.Enabled = true;
+            string s1;
+            button3.Enabled = true;
+
+
+            string[] a = new string[] {"世界高樓之ㄧ台北101高度為何? A:508 B:408 C:500 D:498 請作答?",
+                                       "中正紀念館改為何名? A:馬英九紀念館 B:台灣民主紀念館 C:陳水扁紀念館 D:228紀念館 請作答?",
+                                       "請問 2012年度奧運哪一地方舉辦? A:北京 B:新加坡 C:倫敦 D:雪梨 請作答?",
+                                       "請問 目前全球首富是誰? A:趙建銘 B:郭台銘 C:麥可喬登 D:比爾蓋茲 請作答?",
+                                       "2007年度台灣之光王建明勝投數幾場 A:17 B:18 C:20 D:19 請作答?",
+                                       "請問2008年度奧運是在哪裡舉辦? A:北京 B:新加坡 C:倫敦 D:雪梨 請作答?"
+                                      };
+
+            Random rndobj1 = new Random();
+            int rndNum1 = rndobj1.Next();
+            x1 = rndobj1.Next(1, 6);
+
+            richTextBox3.Text += "x1 = " + x1.ToString() + "\n";
+
+            if (x1 == 1)
+            {
+                s1 = a[0];
+                richTextBox1.Text = s1;
+            }
+            else if (x1 == 2)
+            {
+                s1 = a[1];
+                richTextBox1.Text = s1;
+            }
+            else if (x1 == 3)
+            {
+                s1 = a[2];
+                richTextBox1.Text = s1;
+            }
+            else if (x1 == 4)
+            {
+                s1 = a[3];
+                richTextBox1.Text = s1;
+            }
+            else if (x1 == 5)
+            {
+                s1 = a[4];
+                richTextBox1.Text = s1;
+            }
+            else if (x1 == 6)
+            {
+                s1 = a[5];
+                richTextBox1.Text = s1;
+            }
+
+            if (right_num == 6)
+            {
+                timer4.Enabled = true;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Visible = true;
+                richTextBox1.ReadOnly = true;
+                timer3.Enabled = true;
+             
+                f1.NAMESCORE = Sname + "|" + score.ToString();
+            }
+            timer2.Enabled = true;
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (namescore != null)
+            {
+                name_score.Add(namescore , null);
+                namescore = null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Visible = true;
+            groupBox1.Visible = true;
+            groupBox2.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+
+            cnt_name = (cnt_name < 5) ? cnt_name + 1 : 1;
+            right_num = 0;
+            score = 0;
+            richTextBox1.Text = string.Empty;
+
+            timer1.Enabled = true;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+
+            a = 10;
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
