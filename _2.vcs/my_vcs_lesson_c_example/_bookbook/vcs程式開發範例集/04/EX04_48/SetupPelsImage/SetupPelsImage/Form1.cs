2da6ddb77885906377f52e31288f6933d90c5181
david <david@insighteyes.com>
2021-07-12 19:19:52 +0800

    david modify vcs code

--- _2.vcs/my_vcs_lesson_c_example/_bookbook/vcs程式開發範例集/04/EX04_48/SetupPelsImage/SetupPelsImage/Form1.cs
+++ _2.vcs/my_vcs_lesson_c_example/_bookbook/vcs程式開發範例集/04/EX04_48/SetupPelsImage/SetupPelsImage/Form1.cs
@@ -11,16 +11,21 @@ namespace SetupPelsImage
 {
     public partial class Form1 : Form
     {
+        int Var_MouseX = 0;//記錄滑鼠游標的X座標的位置
+        int Var_MouseY = 0;//記錄滑鼠游標的X座標的位置
+        int W = 0;//記錄滑鼠游標拖曳矩形框的寬度
+        int H = 0;//記錄滑鼠游標拖曳矩形框的高度
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        int Var_MouseX = 0;//記錄滑鼠游標的X座標的位置
-        int Var_MouseY = 0;//記錄滑鼠游標的X座標的位置
-        int Var_MouseW = 0;//記錄滑鼠游標拖曳矩形框的寬度
-        int Var_MouseH = 0;//記錄滑鼠游標拖曳矩形框的高度
-        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
             Var_MouseX = e.X;//記錄滑鼠游標按下時的X座標值
@@ -29,22 +34,28 @@ namespace SetupPelsImage
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
-            Var_MouseW = e.X - Var_MouseX;//取得滑鼠游標拖曳矩形框的寬度
-            Var_MouseH = e.Y - Var_MouseY;//取得滑鼠游標拖曳矩形框的高度
-            if (Var_MouseW == 0)//矩形框的寬度為0
-                Var_MouseW = 1;//設定寬度為1
-            if (Var_MouseH == 0)//矩形框的高度為0
-                Var_MouseH = 1;//設定高度為1
+            W = e.X - Var_MouseX;//取得滑鼠游標拖曳矩形框的寬度
+            H = e.Y - Var_MouseY;//取得滑鼠游標拖曳矩形框的高度
+            if (W == 0)//矩形框的寬度為0
+            {
+                W = 1;//設定寬度為1
+            }
+            if (H == 0)//矩形框的高度為0
+            {
+                H = 1;//設定高度為1
+            }
             label_X.Text = Var_MouseX.ToString();//顯示X的座標值
             label_Y.Text = Var_MouseY.ToString();//顯示Y的座標值
-            label_W.Text = Var_MouseW.ToString();//顯示寬度
-            label_H.Text = Var_MouseH.ToString();//顯示高度
+            label_W.Text = W.ToString();//顯示寬度
+            label_H.Text = H.ToString();//顯示高度
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             if (colorDialog1.ShowDialog() == DialogResult.OK)//開啟顏色對話框
+            {
                 panel1.BackColor = colorDialog1.Color;//顯示選擇的顏色
+            }
         }
 
         public Image SetPels(PictureBox Pict, Panel panel, Color color, int x, int y, int w, int h)
@@ -53,17 +64,25 @@ namespace SetupPelsImage
             Bitmap Var_SaveBmp = (Bitmap)Pict.Image;//實例化Bitmap
             //搜尋矩形框內的各象素點
             for (int i = x; i < x + w; i++)
+            {
                 for (int j = y; j < y + h; j++)
                 {
                     Var_SaveBmp.SetPixel(i, j, color);//設定目前象素點的顏色
                 }
+            }
             return Var_SaveBmp;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //呼叫自定義方法修改圖片的顏色
-            pictureBox1.Image = SetPels(pictureBox1, panel1, panel1.BackColor, Var_MouseX, Var_MouseY, Var_MouseW, Var_MouseH);
+            if ((W > 0) && (H > 0))
+            {
+                //呼叫自定義方法修改圖片的顏色
+                pictureBox1.Image = SetPels(pictureBox1, panel1, panel1.BackColor, Var_MouseX, Var_MouseY, W, H);
+            }
         }
+
     }
 }
+
+
--- _2.vcs/my_vcs_lesson_c_example/_bookbook/vcs程式開發範例集/05/Ex05_04/AutomaticPlayImplement/AutomaticPlayImplement/Form1.cs
+++ _2.vcs/my_vcs_lesson_c_example/_bookbook/vcs程式開發範例集/05/Ex05_04/AutomaticPlayImplement/AutomaticPlayImplement/Form1.cs
@@ -25,11 +25,18 @@ namespace AutomaticPlayImplement
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            bt_clear.Location = new Point(richTextBox1.Location.X + richTextBox1.Size.Width - bt_clear.Size.Width, richTextBox1.Location.Y + richTextBox1.Size.Height - bt_clear.Size.Height);
+        }
+
+        private void bt_clear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
         }
 
         public void GetAllFiles(DirectoryInfo dir)
         {
-            this.listBox1.Items.Clear();
+            //richTextBox1.Text += "clear 1\n";
+            //this.listBox1.Items.Clear();
             FileSystemInfo[] fileinfo = dir.GetFileSystemInfos();
             foreach (FileSystemInfo i in fileinfo)
             {
@@ -48,6 +55,7 @@ namespace AutomaticPlayImplement
                         //添加列表
                         WC = new WMPLib.WindowsMediaPlayerClass();
                         MC = WC.newMedia(str);
+                        richTextBox1.Text += "add " + str + "\n";
                         this.axWindowsMediaPlayer1.currentPlaylist.appendItem(MC);
                     }
                 }
@@ -55,6 +63,7 @@ namespace AutomaticPlayImplement
         }
         private void button1_Click(object sender, EventArgs e)
         {
+            richTextBox1.Text += "clear 2\n";
             this.listBox1.Items.Clear();
             DirectoryInfo dir = new DirectoryInfo(foldername);
             GetAllFiles(dir);
@@ -77,11 +86,30 @@ namespace AutomaticPlayImplement
 
         private void button4_Click(object sender, EventArgs e)
         {
+            this.axWindowsMediaPlayer1.Ctlcontrols.next();
+
+            //richTextBox1.Text += this.axWindowsMediaPlayer1.URL + "\n";
+
+            richTextBox1.Text += "下一首\t";
+            richTextBox1.Text += this.axWindowsMediaPlayer1.Ctlcontrols.currentItem.name + "\t";
+            richTextBox1.Text += this.axWindowsMediaPlayer1.Ctlcontrols.currentItem.sourceURL + "\n";
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.axWindowsMediaPlayer1.Ctlcontrols.previous();
+
+            //richTextBox1.Text += this.axWindowsMediaPlayer1.URL + "\n";
+
+            richTextBox1.Text += "上一首\t";
+            richTextBox1.Text += this.axWindowsMediaPlayer1.Ctlcontrols.currentItem.name + "\t";
+            richTextBox1.Text += this.axWindowsMediaPlayer1.Ctlcontrols.currentItem.sourceURL + "\n";
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            /*
+            richTextBox1.Text += "選中第 " + listBox1.SelectedIndex.ToString() + " 首\n";
             //停止播放
             this.axWindowsMediaPlayer1.Ctlcontrols.stop();
             richTextBox1.Text += "select : " + listBox1.SelectedItem + "\n";
@@ -89,7 +117,6 @@ namespace AutomaticPlayImplement
             this.axWindowsMediaPlayer1.URL = foldername + listBox1.SelectedItem;
             richTextBox1.Text += "url : " + this.axWindowsMediaPlayer1.URL + "\n";
             this.axWindowsMediaPlayer1.Ctlcontrols.play();
-            */
         }
 
         private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
@@ -101,7 +128,27 @@ namespace AutomaticPlayImplement
             this.axWindowsMediaPlayer1.URL = foldername + listBox1.SelectedItem;
             richTextBox1.Text += "url : " + this.axWindowsMediaPlayer1.URL + "\n";
             this.axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
 
+        private void button6_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "info\t";
+            richTextBox1.Text += this.axWindowsMediaPlayer1.Ctlcontrols.currentItem.name + "\t";
+            richTextBox1.Text += this.axWindowsMediaPlayer1.Ctlcontrols.currentItem.sourceURL + "\n";
+
+            //目前再WMP內總共有幾首歌
+            //每首歌印出來
+            richTextBox1.Text += "count = " + this.axWindowsMediaPlayer1.Controls.Count.ToString() + "\n";
+
+            int count = this.axWindowsMediaPlayer1.currentPlaylist.count;
+            richTextBox1.Text += "播放清單內共有 : " + count.ToString() + " 首歌\n";
+            int i;
+            for (i = 0; i < count; i++)
+            {
+                richTextBox1.Text += i.ToString() + "\t";
+                richTextBox1.Text += this.axWindowsMediaPlayer1.currentPlaylist.Item[i].name + "\t";
+                richTextBox1.Text += this.axWindowsMediaPlayer1.currentPlaylist.Item[i].sourceURL + "\n";
+            }
         }
     }
 }
