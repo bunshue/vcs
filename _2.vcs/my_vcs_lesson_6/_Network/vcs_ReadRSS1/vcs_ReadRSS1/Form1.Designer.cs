10e5b83b4b3ec2f53b960d4c0211f92e3cf388d9
david <david@insighteyes.com>
2021-11-11 19:31:05 +0800

    dvd

---
+++ _2.vcs/my_vcs_lesson_6/_Network/vcs_ReadRSS1/vcs_ReadRSS1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace vcs_ReadRSS1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(632, 30);
+            this.textBox1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.treeView1.Location = new System.Drawing.Point(12, 59);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(632, 558);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(669, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.richTextBox1.Location = new System.Drawing.Point(670, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(332, 558);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 629);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ _2.vcs/my_vcs_lesson_6/_Network/vcs_ReadRSS1/vcs_ReadRSS1/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Xml;
+using System.Collections;   //for Hashtable
+
+namespace vcs_ReadRSS1
+{
+    public partial class Form1 : Form
+    {
+        public class rss
+        {
+            public struct Channel
+            {
+                public string Title;
+                public Hashtable Items;
+            }
+
+            public struct Item
+            {
+                public string Title;
+                public string Description;
+                public string Link;
+                public string author;
+                public string pubdate;
+            }
+        }
+        //Channel下面包含Title和Item两个变量，TItle存的是该RSS源的名称，Item存放的是该RSS源中的新闻列表，Item里面的每一个变量都是Struct Item类型的。
+
+        //先写了一个FoundChildNode（XmlNode Node, string Name）函数去查找指定Node结点下名称为Name的子节点
+        private XmlNode FoundChildNode(XmlNode Node, string Name)
+        {
+            XmlNode childlNode = null;
+            for (int i = 0; i < Node.ChildNodes.Count; i++)
+            {
+                if (Node.ChildNodes[i].Name == Name && Node.ChildNodes[i].ChildNodes.Count > 0)
+                {
+                    childlNode = Node.ChildNodes[i];
+                    return childlNode;
+                }
+            }
+            return childlNode;
+        }
+
+        //getRssItem函数是遍历所有Item结点的子节点，然后将相应的内容存入Item中，也就是将我们需要的新闻标题，时间，描述等等存入Item中。
+        private rss.Item getRssItem(XmlNode Node)
+        {
+            rss.Item item = new rss.Item();
+            for (int i = 0; i < Node.ChildNodes.Count; i++)
+            {
+                if (Node.ChildNodes[i].Name == "title")
+                {
+                    item.Title = Node.ChildNodes[i].InnerText;
+                }
+                else if (Node.ChildNodes[i].Name == "description")
+                {
+                    item.Description = Node.ChildNodes[i].InnerText;
+                }
+                else if (Node.ChildNodes[i].Name == "link")
+                {
+                    item.Link = Node.ChildNodes[i].InnerText;
+                }
+                else if (Node.ChildNodes[i].Name == "author")
+                {
+                    item.author = Node.ChildNodes[i].InnerText;
+                }
+                else if (Node.ChildNodes[i].Name == "pubdate")
+                {
+                    item.pubdate = Node.ChildNodes[i].InnerText;
+                }
+            }
+            return item;
+        }
+
+        public rss.Channel ReadXml()
+        {
+            string url = @"https://www.mohw.gov.tw/rss-16-1.html";
+            XmlTextReader Reader = new XmlTextReader(url);
+            XmlValidatingReader Valid = new XmlValidatingReader(Reader);
+            Valid.ValidationType = ValidationType.None;
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Reader);
+
+            XmlNode rssNode = FoundChildNode(xmlDoc, "rss");
+            XmlNode channelNode = FoundChildNode(rssNode, "channel");
+
+            //然后我们就可以遍历它的子节点，根据子节点的Name属性，读取我们需要的信息。
+            rss.Channel channel = new rss.Channel();
+            channel.Items = new Hashtable();
+            for (int i = 0; i < channelNode.ChildNodes.Count; i++)
+            {
+                switch (channelNode.ChildNodes[i].Name)
+                {
+                    case "title":
+                        {
+                            channel.Title = channelNode.ChildNodes[i].InnerText;
+                            break;
+                        }
+                    case "item":
+                        {
+                            rss.Item item = this.getRssItem(channelNode.ChildNodes[i]);
+                            channel.Items.Add(channel.Items.Count, item);
+                            break;
+                        }
+                }
+            }
+            return channel;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServicePointManager.SecurityProtocol = Protocols.protocol_Tls11 | Protocols.protocol_Tls12;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rss.Channel readChannel = new rss.Channel();
+            readChannel = ReadXml();
+
+            ViewRss(readChannel);
+
+            /*
+            Console.WriteLine(readChannel.Title);
+            for (int i = 0; i < readChannel.Items.Count; i++)
+            {
+                rss.Item item = (rss.Item)readChannel.Items[i];
+                Console.WriteLine(item.Title);
+                Console.WriteLine(item.Link);
+                Console.WriteLine(item.Description);
+                Console.WriteLine(item.author);
+                Console.WriteLine(item.pubdate);
+            }
+            */
+        }
+
+        private void ViewRss(rss.Channel channel)
+        {
+            treeView1.BeginUpdate();
+            treeView1.Nodes.Clear();
+            TreeNode channelNode = treeView1.Nodes.Add(channel.Title);
+            channelNode.Tag = "";
+            for (int i = 0; i < channel.Items.Count; i++)
+            {
+                rss.Item item = (rss.Item)channel.Items[i];
+                TreeNode itemNode = channelNode.Nodes.Add(item.Title);
+                itemNode.Tag = item.Link;
+            }
+            treeView1.ExpandAll();
+            treeView1.EndUpdate();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode itemNode = e.Node;
+            string URL = itemNode.Tag.ToString();
+            if (URL.Length != 0)
+            {
+                System.Diagnostics.Process.Start(URL);
+            }
+        }
+    }
+
+    public class Protocols
+    {
+        public const SecurityProtocolType
+            protocol_SystemDefault = 0,
+            protocol_Ssl3 = (SecurityProtocolType)48,
+            protocol_Tls = (SecurityProtocolType)192,
+            protocol_Tls11 = (SecurityProtocolType)768,
+            protocol_Tls12 = (SecurityProtocolType)3072;
+    }
+
+}
+
+
