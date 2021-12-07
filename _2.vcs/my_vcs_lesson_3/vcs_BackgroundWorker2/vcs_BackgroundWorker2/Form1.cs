b15fa123ccb2cf4864c92fb3370c33e90f93a8a8
david <david@insighteyes.com>
2021-12-07 09:48:40 +0800

    david modify vcs code

---
+++ _2.vcs/my_vcs_lesson_3/vcs_BackgroundWorker2/vcs_BackgroundWorker2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Diagnostics;
+
+namespace vcs_BackgroundWorker2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //C# 跨 Thread 存取 UI
+            Form1.CheckForIllegalCrossThreadCalls = false;  //解決跨執行緒控制無效
+            Control.CheckForIllegalCrossThreadCalls = false;//忽略跨執行緒錯誤
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string mesg = (e.Argument as string[])[0];
+            richTextBox1.Text += "取得訊息 " + mesg + "\n";
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            richTextBox1.Text += "完成\n";
+
+        }
+
+        int cnt = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = "message " + cnt.ToString();
+            backgroundWorker1.RunWorkerAsync(new string[1] { message });
+            cnt++;
+        }
+
+    }
+}
---
+++ _2.vcs/my_vcs_lesson_6/_ReadWriteFile/vcs_ReadWrite_PDF3/vcs_ReadWrite_PDF3/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+//參考/加入參考/PdfSharp.dll
+
+using System.IO;
+using System.Diagnostics;
+
+using PdfSharp;
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+
+namespace vcs_ReadWrite_PDF3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filename1 = @"C:\______test_files\picture1.jpg";
+                //string filename2 = @"aaaaa.pdf";
+
+                PdfDocument doc = new PdfDocument();
+                doc.Pages.Add(new PdfPage());
+                XGraphics xgr = XGraphics.FromPdfPage(doc.Pages[0]);
+                XImage img = XImage.FromFile(filename1);
+
+                xgr.DrawImage(img, 0, 0);
+
+                string filename2 = Application.StartupPath + "\\pdf_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".pdf";
+
+                try
+                {
+
+                    doc.Save(filename2);
+                    richTextBox1.Text += "已存檔 : " + filename2 + "\n";
+                }
+                catch (Exception ex)
+                {
+                    richTextBox1.Text += "錯誤訊息 : " + ex.Message + "\n";
+                }
+                doc.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
