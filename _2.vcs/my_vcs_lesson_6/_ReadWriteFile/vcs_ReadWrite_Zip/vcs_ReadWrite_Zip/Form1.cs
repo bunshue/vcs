9800c1f3ffad7467436b635ac7ffef986631bab6
david <david@insighteyes.com>
2022-08-25 19:31:05 +0800

    david modify vcs code

---
+++ _2.vcs/my_vcs_lesson_6/_ReadWriteFile/vcs_ReadWrite_Zip/vcs_ReadWrite_Zip/Form1.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+// Use NuGet Manager to install System.IO.Compression.
+// Add references to:
+//      System.IO.Compression
+//      System.IO.Compression.FileSystem
+
+using System.IO.Compression;
+
+namespace vcs_ReadWrite_Zip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //À£ÁYÀÉ®×
+
+            List<String> filenames = new List<String>();
+
+            string filename1 = @"C:\______test_files\picture1.jpg";
+            string filename2 = @"C:\______test_files\picture2.jpg";
+            string filename3 = @"C:\______test_files\elephant.jpg";
+            string filename4 = @"C:\______test_files\bear.jpg";
+
+            filenames.Clear();
+            filenames.Add(filename1);
+            filenames.Add(filename2);
+            filenames.Add(filename3);
+            filenames.Add(filename4);
+
+            string zip_filename = Application.StartupPath + "\\Zip_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".zip";
+
+            // Create the Zip archive for update.
+            using (ZipArchive archive = ZipFile.Open(zip_filename, ZipArchiveMode.Update))
+            {
+                // Add the files to the archive.
+                foreach (string filename in filenames)
+                {
+                    FileInfo file_info = new FileInfo(filename);
+
+                    // Make an entry name for the file.
+                    string entry_name = file_info.Name;
+                    for (int version = 1; version < 10000; version++)
+                    {
+                        ZipArchiveEntry old_entry = archive.GetEntry(entry_name);
+                        if (old_entry == null) break;
+                        entry_name = Path.GetFileNameWithoutExtension(file_info.Name) + file_info.Extension;
+                    }
+
+                    // Add the file to this entry.
+                    archive.CreateEntryFromFile(filename, entry_name);
+                    richTextBox1.Text += "filename = " + filename + "\n";
+                    richTextBox1.Text += "entry_name = " + entry_name + "\n";
+                }
+                richTextBox1.Text += "zip_file_name = " + zip_filename + "\n";
+                richTextBox1.Text += "Done\n";
+            }
+        }
+    }
+}
