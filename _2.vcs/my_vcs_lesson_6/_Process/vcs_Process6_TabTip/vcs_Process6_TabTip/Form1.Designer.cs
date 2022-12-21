161a88cda7a99c2861f69dec72a5e404791f991a
david <david@insighteyes.com>
2022-12-21 15:07:07 +0800

    david modify vcs code

---
+++ _2.vcs/my_vcs_lesson_6/_Process/vcs_Process6_TabTip/vcs_Process6_TabTip/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace vcs_Process6_TabTip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(125, 151);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(942, 52);
+            this.richTextBox3.TabIndex = 11;
+            this.richTextBox3.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 151);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 52);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "輸入資料";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(125, 84);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(942, 52);
+            this.richTextBox2.TabIndex = 9;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 52);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "輸入資料";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(125, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(942, 52);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 52);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "輸入資料";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 495);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ _2.vcs/my_vcs_lesson_6/_Process/vcs_Process6_TabTip/vcs_Process6_TabTip/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Diagnostics;
+
+namespace vcs_Process6_TabTip
+{
+    public partial class Form1 : Form
+    {
+        string progFiles = string.Empty;
+        string keyboardPath = string.Empty;
+        Process myProcess = new Process();
+
+        int focus_at = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progFiles = @"C:\Program Files\Common Files\Microsoft Shared\ink";
+            keyboardPath = Path.Combine(progFiles, "TabTip.exe");
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (focus_at == 1)
+            {
+                richTextBox1.Focus();
+            }
+            else if (focus_at == 2)
+            {
+                richTextBox2.Focus();
+            }
+            else if (focus_at == 3)
+            {
+                richTextBox3.Focus();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            focus_at = 1;
+            //System.Diagnostics.Process.Start("" + System.Environment.SystemDirectory + "/osk.exe");
+            //OR
+            //System.Diagnostics.Process.Start("osk.exe");
+
+
+            myProcess = Process.Start(keyboardPath);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            focus_at = 2;
+            myProcess = Process.Start(keyboardPath);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            focus_at = 3;
+            myProcess = Process.Start(keyboardPath);
+        }
+    }
+}
