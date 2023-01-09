f11668e3ced3656919d9dfa0110c4802520c2ced
david <david@insighteyes.com>
2023-01-09 20:29:17 +0800

    david modify vcs code

---
+++ _2.vcs/my_vcs_lesson_6/_Clock/vcs_Clock1/vcs_Clock1/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace vcs_Clock1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(390, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(334, 590);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 614);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ _2.vcs/my_vcs_lesson_6/_Clock/vcs_Clock1/vcs_Clock1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace vcs_Clock1
+{
+    public partial class Form1 : Form
+    {
+
+        int flag_operation_mode = MODE_0;
+
+        private const int MODE_0 = 0x00;   //時間模式
+        private const int MODE_1 = 0x01;   //碼表模式
+        private const int MODE_2 = 0x02;   //離開
+        private const int MODE_3 = 0x03;   //
+        private const int MODE_4 = 0x04;   //
+        private const int MODE_5 = 0x05;   //最上層切換
+
+        private const int S_OK = 0;     //system return OK
+        private const int S_FALSE = 1;     //system return FALSE
+        private const int W = 200;
+        private const int H = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //string dt = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            //string dt = DateTime.Now.ToString();
+            //string dt = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+            string dt = DateTime.Now.ToString("HH:mm:ss");
+            Graphics g = e.Graphics;
+
+            Brush b = new SolidBrush(Color.Blue);
+            Font f = new Font("標楷體", 20, FontStyle.Bold);
+            Pen p = new Pen(Brushes.Blue, 5);
+
+            g.DrawRectangle(p, 0, 0, W, H);
+            g.DrawString(dt, f, b, 10, 10);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Text += "(" + e.X.ToString() + ", " + e.Y.ToString() + ") ";
+            int xx = e.X;
+            int yy = e.Y;
+            if (yy < H / 2) //上排
+            {
+                if (xx < W / 3) //左
+                {
+                    richTextBox1.Text += "零";
+                    flag_operation_mode = MODE_0;
+                }
+                else if (xx < W * 2 / 3) //中
+                {
+                    richTextBox1.Text += "一";
+                    flag_operation_mode = MODE_1;
+
+                }
+                else  //右
+                {
+                    richTextBox1.Text += "二";
+                    flag_operation_mode = MODE_2;
+                    Application.Exit();
+                }
+            }
+            else   //下排
+            {
+                if (xx < W / 3) //左
+                {
+                    richTextBox1.Text += "三";
+                    flag_operation_mode = MODE_3;
+                }
+                else if (xx < W * 2 / 3) //中
+                {
+                    richTextBox1.Text += "四";
+                    flag_operation_mode = MODE_4;
+
+                }
+                else  //右
+                {
+                    richTextBox1.Text += "五";
+                    flag_operation_mode = MODE_5;
+                    if (this.TopMost == false)
+                        this.TopMost = true;
+                    else
+                        this.TopMost = false;
+                }
+
+            }
+
+
+
+        }
+    }
+}
