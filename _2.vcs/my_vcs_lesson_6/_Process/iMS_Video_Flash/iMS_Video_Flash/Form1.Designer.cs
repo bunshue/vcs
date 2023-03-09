f62cf4b18a65ea7fd94ec2922d496dbebe05659e
david <david@insighteyes.com>
2023-03-09 17:26:00 +0800

    david modify vcs code

---
+++ _2.vcs/my_vcs_lesson_6/_Process/iMS_Video_Flash/iMS_Video_Flash/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace iMS_Video_Flash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.bt_clear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button0);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(223, 499);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(111, 84);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "覆蓋舊檔";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(43, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 60);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "刪除映像檔";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(43, 335);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 60);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "映像檔資訊";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(43, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 60);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "製作並燒錄映像檔";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(43, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "燒錄映像檔";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button0
+            // 
+            this.button0.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button0.Location = new System.Drawing.Point(43, 100);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(140, 60);
+            this.button0.TabIndex = 1;
+            this.button0.Text = "製作映像檔";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(43, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 36);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "BOOT.bin";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(264, 66);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 100);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // bt_clear
+            // 
+            this.bt_clear.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.bt_clear.Location = new System.Drawing.Point(287, 96);
+            this.bt_clear.Name = "bt_clear";
+            this.bt_clear.Size = new System.Drawing.Size(67, 42);
+            this.bt_clear.TabIndex = 74;
+            this.bt_clear.Text = "clear";
+            this.bt_clear.UseVisualStyleBackColor = true;
+            this.bt_clear.Click += new System.EventHandler(this.bt_clear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(19, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 21);
+            this.label1.TabIndex = 75;
+            this.label1.Text = "label1";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.Location = new System.Drawing.Point(17, 62);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(60, 60);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "選取檔案";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button6.Location = new System.Drawing.Point(83, 62);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(60, 60);
+            this.button6.TabIndex = 76;
+            this.button6.Text = "燒錄";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Location = new System.Drawing.Point(241, 329);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(154, 139);
+            this.groupBox2.TabIndex = 77;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "燒錄指定檔案";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 520);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.bt_clear);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Aries映像檔燒錄程式";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button bt_clear;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ _2.vcs/my_vcs_lesson_6/_Process/iMS_Video_Flash/iMS_Video_Flash/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Diagnostics;
+
+namespace iMS_Video_Flash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;//忽略跨執行緒錯誤
+
+            show_item_location();
+        }
+
+        void show_item_location()
+        {
+            int x_st;
+            int y_st;
+            int dx;
+            int dy;
+            int w;
+            int h;
+
+            w = 140;
+            h = 60;
+
+            //button
+            x_st = 20;
+            y_st = 20;
+            dx = w + 60;
+            dy = h + 30;
+
+            groupBox1.Location = new Point(x_st + dx * 0, y_st + dy * 1);
+            textBox1.Location = new Point(x_st + dx * 0, y_st + dy * 0 + 10);
+            checkBox1.Location = new Point(x_st + dx * 0 + 70, y_st + dy * 1 - 30);
+            button0.Location = new Point(x_st + dx * 0, y_st + dy * 1);
+            button1.Location = new Point(x_st + dx * 0, y_st + dy * 2);
+            button2.Location = new Point(x_st + dx * 0, y_st + dy * 3);
+            button3.Location = new Point(x_st + dx * 0, y_st + dy * 4);
+            button4.Location = new Point(x_st + dx * 0, y_st + dy * 5);
+
+            richTextBox1.Location = new Point(x_st + dx * 1, y_st + dy * 1);
+
+            groupBox1.Size = new Size(20 + w + 20, 550);
+            richTextBox1.Size += new Size(1200, 540);
+            this.Size = new Size(1550, 800);
+
+            label1.Text = "";
+            groupBox2.Location = new Point(x_st + dx * 0, y_st + dy * 0 - 10);
+            groupBox2.Size = new Size(800, 90);
+            button5.Location = new Point(x_st + dx * 0, y_st + dy * 0);
+            button6.Location = new Point(x_st + dx * 0 + 80 * 1, y_st + dy * 0);
+            label1.Location = new Point(x_st + dx * 0 + 80 * 2, y_st + dy * 0 + 15);
+
+            bt_clear.Location = new Point(richTextBox1.Location.X + richTextBox1.Size.Width - bt_clear.Size.Width, richTextBox1.Location.Y + richTextBox1.Size.Height - bt_clear.Size.Height);
+        }
+
+        private void bt_clear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            string filename = @"C:\_git\ims1\iMS_Video\iMS_Video.sdk\" + textBox1.Text;
+
+            //檢查映像檔
+            if (checkBox1.Checked == false)
+            {
+                if (File.Exists(filename) == true)            //確認檔案是否存在
+                {
+                    richTextBox1.Text += "映像檔 : " + filename + " 已存在, 無法重新建立\n";
+                    return;
+                }
+            }
+
+            //製作映像檔
+            richTextBox1.Text += "製作映像檔\n";
+            string exe_filename = "cmd";
+            //string parameters = @" /C C:\Xilinx\SDK\2019.1\bin\bootgen -image C:\_git\ims1\iMS_Video\iMS_Video.sdk\output.bif -arch zynq -o C:\_git\ims1\iMS_Video\iMS_Video.sdk\BOOT.bin";
+            string parameters = @" /C C:\Xilinx\SDK\2019.1\bin\bootgen -image C:\_git\ims1\iMS_Video\iMS_Video.sdk\output.bif -arch zynq -o " + filename;
+
+            if (checkBox1.Checked == true)
+            {
+                parameters += " -w on";
+            }
+
+            run_command_line_process_async(exe_filename, parameters);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                richTextBox1.Text += "未設定燒錄檔檔名\n";
+                return;
+            }
+
+            //燒錄映像檔
+            string exe_filename = "cmd";
+            //string parameters = @" /C C:\Xilinx\SDK\2019.1\bin\program_flash -f C:\_git\ims1\iMS_Video\iMS_Video.sdk\BOOT.bin -offset 0 -flash_type qspi-x1-single -fsbl C:\_git\ims1\iMS_Video\iMS_Video.sdk\aries\Release\aries.elf -cable type xilinx_tcf url TCP:127.0.0.1:3121";
+            string parameters = @" /C C:\Xilinx\SDK\2019.1\bin\program_flash -f C:\_git\ims1\iMS_Video\iMS_Video.sdk\" + textBox1.Text + @" -offset 0 -flash_type qspi-x1-single -fsbl C:\_git\ims1\iMS_Video\iMS_Video.sdk\aries\Release\aries.elf -cable type xilinx_tcf url TCP:127.0.0.1:3121";
+            //richTextBox1.Text += parameters + "\n";
+
+            run_command_line_process_async(exe_filename, parameters);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //製作並燒錄映像檔
+
+            string filename = @"C:\_git\ims1\iMS_Video\iMS_Video.sdk\" + textBox1.Text;
+
+            //檢查映像檔
+            if (checkBox1.Checked == false)
+            {
+                if (File.Exists(filename) == true)            //確認檔案是否存在
+                {
+                    richTextBox1.Text += "映像檔 : " + filename + " 已存在, 無法重新建立\n";
+                    return;
+                }
+            }
+
+            //製作映像檔
+            richTextBox1.Text += "製作映像檔\n";
+            string exe_filename = "cmd";
+            //string parameters = @" /C C:\Xilinx\SDK\2019.1\bin\bootgen -image C:\_git\ims1\iMS_Video\iMS_Video.sdk\output.bif -arch zynq -o C:\_git\ims1\iMS_Video\iMS_Video.sdk\BOOT.bin";
+            string parameters = @" /C C:\Xilinx\SDK\2019.1\bin\bootgen -image C:\_git\ims1\iMS_Video\iMS_Video.sdk\output.bif -arch zynq -o " + filename;
+
+            if (checkBox1.Checked == true)
+            {
+                parameters += " -w on";
+            }
+
+            run_command_line_process_async(exe_filename, parameters);
+
+            //燒錄映像檔
+            //parameters = @" /C C:\Xilinx\SDK\2019.1\bin\program_flash -f C:\_git\ims1\iMS_Video\iMS_Video.sdk\BOOT.bin -offset 0 -flash_type qspi-x1-single -fsbl C:\_git\ims1\iMS_Video\iMS_Video.sdk\aries\Release\aries.elf -cable type xilinx_tcf url TCP:127.0.0.1:3121";
+            parameters = @" /C C:\Xilinx\SDK\2019.1\bin\program_flash -f C:\_git\ims1\iMS_Video\iMS_Video.sdk\" + textBox1.Text + @" -offset 0 -flash_type qspi-x1-single -fsbl C:\_git\ims1\iMS_Video\iMS_Video.sdk\aries\Release\aries.elf -cable type xilinx_tcf url TCP:127.0.0.1:3121";
+            richTextBox1.Text += parameters + "\n";
+
+            run_command_line_process_async(exe_filename, parameters);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //映像檔資訊
+            string exe_filename = "cmd";
+            string parameters = @" /C C:\Xilinx\SDK\2019.1\gnu\aarch32\nt\gcc-arm-none-eabi\bin\arm-none-eabi-size.exe C:\_git\ims1\iMS_Video\iMS_Video.sdk\aries_main\Release\aries_main.elf";
+
+            run_command_line_process_async(exe_filename, parameters);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //刪除映像檔
+
+            string filename = @"C:\_git\ims1\iMS_Video\iMS_Video.sdk\" + textBox1.Text;
+
+            if (File.Exists(filename) == false)     //確認檔案是否存在
+            {
+                richTextBox1.Text += "映像檔 : " + filename + ", 不存在, 無法刪除\n";
+            }
+            else
+            {
+                File.Delete(filename);
+                richTextBox1.Text += "映像檔 : " + filename + ", 已刪除\n";
+            }
+        }
+
+        //非同步 Process使用
+        void run_command_line_process_async(string exe_filename, string command)
+        {
+            //Process process = new Process();    //創建一個進程用於調用外部程序
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = exe_filename, //設定要啟動的程式
+                    Arguments = command,
+
+                    UseShellExecute = false,  //false, 關閉Shell的使用, 是否指定操作系統外殼進程啟動程序, 可能接受來自調用程序的輸入信息
+                    RedirectStandardInput = true, //重定向標準輸入, 可能接受來自調用程序的輸入信息
+                    RedirectStandardOutput = true, //重定向標準輸出, 由調用程序獲取輸出信息
+                    RedirectStandardError = true, //重定向錯誤輸出
+                    CreateNoWindow = true, //true: 設置不顯示程式窗口, false: 出現cmd的黑窗體
+
+                    WindowStyle = ProcessWindowStyle.Hidden
+                },
+            };
+
+            //設定非同步資料處理 output and error handlers
+            process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+            process.ErrorDataReceived += new DataReceivedEventHandler(OutputHandler);
+
+            process.Start();    //啟動程式
+
+            //啟動讀取資料輸出與錯誤輸出
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            richTextBox1.Text += "等待程式結束.......\n";
+            process.WaitForExit();	//等待退出
+            richTextBox1.Text += "程式結束\n";
+        }
+
+        void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            //目前無法做到換行, 也不能操作richTextBox的內容
+            richTextBox1.Text += outLine.Data;
+
+            //跳至最後面 fail
+            //richTextBox1.Focus();
+            //richTextBox1.Select(richTextBox1.Text.Length, 0);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "開啟燒錄檔案";
+            openFileDialog1.FileName = "";              //預設開啟的檔名
+            openFileDialog1.DefaultExt = "*.bin";
+            openFileDialog1.Filter = "bin檔(*.bin)|*.bin";
+            openFileDialog1.InitialDirectory = @"C:\_git\ims2\_release";
+            openFileDialog1.Multiselect = false;    //單選
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                /*
+                richTextBox1.Text += "已選取檔案: " + openFileDialog1.FileName + "\n";
+                FileInfo f = new FileInfo(openFileDialog1.FileName);
+                richTextBox1.Text += "Name: " + f.Name + "\n";
+                richTextBox1.Text += "FullName: " + f.FullName + "\n";
+                richTextBox1.Text += "Extension: " + f.Extension + "\n";
+                richTextBox1.Text += "size: " + f.Length.ToString() + "\n";
+                richTextBox1.Text += "Directory: " + f.Directory + "\n";
+                richTextBox1.Text += "DirectoryName: " + f.DirectoryName + "\n";
+                */
+                label1.Text = openFileDialog1.FileName;
+            }
+            else
+            {
+                label1.Text = "";
+            }
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "")
+            {
+                richTextBox1.Text += "未選取燒錄檔\n";
+                return;
+            }
+
+            //燒錄映像檔
+            string exe_filename = "cmd";
+            //string parameters = @" /C C:\Xilinx\SDK\2019.1\bin\program_flash -f C:\_git\ims1\iMS_Video\iMS_Video.sdk\BOOT.bin -offset 0 -flash_type qspi-x1-single -fsbl C:\_git\ims1\iMS_Video\iMS_Video.sdk\aries\Release\aries.elf -cable type xilinx_tcf url TCP:127.0.0.1:3121";
+            string parameters = @" /C C:\Xilinx\SDK\2019.1\bin\program_flash -f " + label1.Text + @" -offset 0 -flash_type qspi-x1-single -fsbl C:\_git\ims1\iMS_Video\iMS_Video.sdk\aries\Release\aries.elf -cable type xilinx_tcf url TCP:127.0.0.1:3121";
+            //richTextBox1.Text += parameters + "\n";
+
+            run_command_line_process_async(exe_filename, parameters);
+        }
+    }
+}
