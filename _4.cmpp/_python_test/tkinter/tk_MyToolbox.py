a4ae2616ba731488f901c225be7529535f78c9fc
david <david@insighteyes.com>
2023-03-20 14:28:03 +0800

    david modify vcs code

--- _4.cmpp/_python_test/tkinter/tk_MyToolbox.py
+++ _4.cmpp/_python_test/tkinter/tk_MyToolbox.py
@@ -1,3 +1,8 @@
+def execute_command():
+    print('execute_command')
+    #回傳結果
+    show_mesg.set("執行命令 : \n")
+
 import tkinter as tk
 
 window = tk.Tk()
@@ -12,12 +17,31 @@ window.geometry(size)
 title = "MyToolbox"
 window.title(title)
 
+show_mesg = tk.StringVar()
 
+font_size = 24
+w = 40
+h = 30
+
+# 設定 Label 
+label1 = tk.Label(window, text="開啟程式", foreground="red", font=("標楷體", font_size), padx = w, pady = h)
+label1.pack()
+
+# 設定 Button
+button1 = tk.Button(window, text="imsLink", foreground="blue", font=("標楷體", font_size), padx = w, pady = h, command = execute_command)
+button1.pack()
+button2 = tk.Button(window, text="imsCap", foreground="blue", font=("標楷體", font_size), padx = w, pady = h, command = execute_command)
+button2.pack()
+button3 = tk.Button(window, text="imsFlash", foreground="blue", font=("標楷體", font_size), padx = w, pady = h, command = execute_command)
+button3.pack()
+
+# 設定 Label
+label3 = tk.Label(window, textvariable=show_mesg, foreground="red", font=("標楷體", font_size), padx = w, pady = h)
+label3.pack()
 
 
 window.mainloop()
 
-window.destroy() # optional; see description below
 
 
 
