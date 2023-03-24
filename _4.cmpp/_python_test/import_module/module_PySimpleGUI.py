56682243f72b0ed3c9dd5f672ea107533d0dc5cd
david <david@insighteyes.com>
2023-03-24 15:50:22 +0800

    david modify vcs code

--- _4.cmpp/_python_test/import_module/module_PySimpleGUI.py
+++ _4.cmpp/_python_test/import_module/module_PySimpleGUI.py
@@ -1,17 +1,30 @@
 import PySimpleGUI as sg
 
-layout=[
-[sg.Text('第一行'), sg.Text('第一行'), sg.Text('第一行')],
-[sg.Button('Y'), sg.Button('N')]
+layout = [
+    [sg.Text('第一行'), sg.Text('第一行'), sg.Text('第一行')],
+    [sg.Button('Y'), sg.Button('N'), sg.Button('Exit')]
 ]
 
 
-window=sg.Window('這是個標題').Layout(layout)
+#window = sg.Window(title = '這是個標題').Layout(layout)
+#window = sg.Window(title = '這是個標題', layout = [[]], margins = (100,50)).read()
 
-button,value=window.Read()
+#button,value = window.Read()
+
+#window = sg.Window(title = '這是個標題', layout)
+#window = sg.Window("這是個標題").Layout(layout)
+window = sg.Window('這是個標題', layout)
+
+while True:
+    event, values = window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
 
 
 
+'''
 print('button:',button)
 
 print('value:',value)
@@ -34,4 +47,7 @@ button,value=window.Read()
 print('button:',button)
 
 print('value:',value)
+
+'''
+
 
