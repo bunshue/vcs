d991089a257296be5c23c8771d270b09e53d56d7
david <david@insighteyes.com>
2023-05-12 19:48:22 +0800

    david modify vcs code

--- _4.python/disk_directory_file/module_sys_os_ddf.py
+++ _4.python/disk_directory_file/module_sys_os_ddf.py
@@ -122,30 +122,40 @@ if os.path.exists(filename):
 else:
     print(filename + "檔案未建立!")   
 
-import os
+import os.path
+
+print('目前檔案:', __file__)
+
+cur_path=os.path.dirname(__file__) # 取得目前目錄路徑
+print('現在目錄路徑:', cur_path)
+
 filename=os.path.abspath("ospath.py")
+filename = 'C:/_git/vcs/_1.data/______test_files1/human1.jpg'
+
 if os.path.exists(filename):   
+    print('完整路徑名稱:', filename)
+    print('檔案大小:', os.path.getsize(filename))
+
     basename=os.path.basename(filename)
-    print("最後的檔案或路徑名稱：" + basename)
+    print('最後的檔案或路徑名稱:', basename)
     
     dirname=os.path.dirname(filename)
-    print("目前檔案目錄路徑：" + dirname) 
+    print('目前檔案目錄路徑:', dirname) 
     
-    print("是否為目錄：",os.path.isdir(filename))
+    print('是否為目錄:', os.path.isdir(filename))
     
     fullpath,fname=os.path.split(filename)
-    print("目錄路徑：" + fullpath)
-    print("檔名：" + fname)
+    print('目錄路徑:', fullpath)
+    print('檔名:', fname)
     
     Drive,fpath=os.path.splitdrive(filename)
-    print("磁碟機：" + Drive)
-    print("路徑名稱：" + fpath)   
+    print('磁碟機:', Drive)
+    print('路徑名稱:', fpath)   
     
     fullpath = os.path.join(fullpath + "\\" + fname)
-    print("組合路徑= " + fullpath)
-
-
-
+    print('組合路徑: ', fullpath)
+else:
+    print('檔案不存在')
 
 
 
