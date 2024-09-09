84ac7ad84d46896d66f5b7f15d12a87bdbf7eb21
david <david@insighteyes.com>
2024-09-09 15:02:41 +0800

    david modify vcs code

---
+++ _2.vcs/my_vcs_lesson_6/_Video/vcs_WebCam3_Record2/vcs_WebCam3_Record2/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace vcs_WebCam3_Record2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lb_fps = new System.Windows.Forms.Label();
+            this.timer_fps = new System.Windows.Forms.Timer(this.components);
+            this.bt_record_start = new System.Windows.Forms.Button();
+            this.bt_record_stop = new System.Windows.Forms.Button();
+            this.bt_clear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lb_main_mesg = new System.Windows.Forms.Label();
+            this.bt_refresh = new System.Windows.Forms.Button();
+            this.bt_exit = new System.Windows.Forms.Button();
+            this.bt_stop = new System.Windows.Forms.Button();
+            this.bt_snapshot = new System.Windows.Forms.Button();
+            this.bt_start = new System.Windows.Forms.Button();
+            this.timer_display = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(130, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 100);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lb_fps
+            // 
+            this.lb_fps.AutoSize = true;
+            this.lb_fps.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_fps.Location = new System.Drawing.Point(6, 101);
+            this.lb_fps.Name = "lb_fps";
+            this.lb_fps.Size = new System.Drawing.Size(43, 21);
+            this.lb_fps.TabIndex = 20;
+            this.lb_fps.Text = "fps";
+            // 
+            // timer_fps
+            // 
+            this.timer_fps.Enabled = true;
+            this.timer_fps.Interval = 1000;
+            this.timer_fps.Tick += new System.EventHandler(this.timer_fps_Tick);
+            // 
+            // bt_record_start
+            // 
+            this.bt_record_start.Location = new System.Drawing.Point(82, 63);
+            this.bt_record_start.Name = "bt_record_start";
+            this.bt_record_start.Size = new System.Drawing.Size(70, 30);
+            this.bt_record_start.TabIndex = 21;
+            this.bt_record_start.Text = "錄影 開始";
+            this.bt_record_start.UseVisualStyleBackColor = true;
+            this.bt_record_start.Click += new System.EventHandler(this.bt_record_start_Click);
+            // 
+            // bt_record_stop
+            // 
+            this.bt_record_stop.Location = new System.Drawing.Point(153, 64);
+            this.bt_record_stop.Name = "bt_record_stop";
+            this.bt_record_stop.Size = new System.Drawing.Size(70, 30);
+            this.bt_record_stop.TabIndex = 22;
+            this.bt_record_stop.Text = "錄影 停止";
+            this.bt_record_stop.UseVisualStyleBackColor = true;
+            this.bt_record_stop.Click += new System.EventHandler(this.bt_record_stop_Click);
+            // 
+            // bt_clear
+            // 
+            this.bt_clear.Location = new System.Drawing.Point(159, 82);
+            this.bt_clear.Name = "bt_clear";
+            this.bt_clear.Size = new System.Drawing.Size(71, 30);
+            this.bt_clear.TabIndex = 24;
+            this.bt_clear.Text = "Clear";
+            this.bt_clear.UseVisualStyleBackColor = true;
+            this.bt_clear.Click += new System.EventHandler(this.bt_clear_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lb_main_mesg);
+            this.groupBox1.Controls.Add(this.bt_refresh);
+            this.groupBox1.Controls.Add(this.bt_exit);
+            this.groupBox1.Controls.Add(this.bt_stop);
+            this.groupBox1.Controls.Add(this.lb_fps);
+            this.groupBox1.Controls.Add(this.bt_snapshot);
+            this.groupBox1.Controls.Add(this.bt_start);
+            this.groupBox1.Controls.Add(this.bt_record_stop);
+            this.groupBox1.Controls.Add(this.bt_record_start);
+            this.groupBox1.Location = new System.Drawing.Point(4, 116);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(512, 141);
+            this.groupBox1.TabIndex = 25;
+            this.groupBox1.TabStop = false;
+            // 
+            // lb_main_mesg
+            // 
+            this.lb_main_mesg.AutoSize = true;
+            this.lb_main_mesg.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lb_main_mesg.ForeColor = System.Drawing.Color.Red;
+            this.lb_main_mesg.Location = new System.Drawing.Point(55, 101);
+            this.lb_main_mesg.Name = "lb_main_mesg";
+            this.lb_main_mesg.Size = new System.Drawing.Size(154, 24);
+            this.lb_main_mesg.TabIndex = 27;
+            this.lb_main_mesg.Text = "lb_main_mesg";
+            // 
+            // bt_refresh
+            // 
+            this.bt_refresh.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.bt_refresh.Location = new System.Drawing.Point(153, 21);
+            this.bt_refresh.Name = "bt_refresh";
+            this.bt_refresh.Size = new System.Drawing.Size(70, 30);
+            this.bt_refresh.TabIndex = 31;
+            this.bt_refresh.Text = "重抓";
+            this.bt_refresh.UseVisualStyleBackColor = true;
+            this.bt_refresh.Click += new System.EventHandler(this.bt_refresh_Click);
+            // 
+            // bt_exit
+            // 
+            this.bt_exit.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.bt_exit.Location = new System.Drawing.Point(229, 22);
+            this.bt_exit.Name = "bt_exit";
+            this.bt_exit.Size = new System.Drawing.Size(70, 30);
+            this.bt_exit.TabIndex = 30;
+            this.bt_exit.Text = "離開";
+            this.bt_exit.UseVisualStyleBackColor = true;
+            this.bt_exit.Click += new System.EventHandler(this.bt_exit_Click);
+            // 
+            // bt_stop
+            // 
+            this.bt_stop.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.bt_stop.Location = new System.Drawing.Point(82, 21);
+            this.bt_stop.Name = "bt_stop";
+            this.bt_stop.Size = new System.Drawing.Size(70, 30);
+            this.bt_stop.TabIndex = 28;
+            this.bt_stop.Text = "停止";
+            this.bt_stop.UseVisualStyleBackColor = true;
+            this.bt_stop.Click += new System.EventHandler(this.bt_stop_Click);
+            // 
+            // bt_snapshot
+            // 
+            this.bt_snapshot.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.bt_snapshot.Location = new System.Drawing.Point(6, 62);
+            this.bt_snapshot.Name = "bt_snapshot";
+            this.bt_snapshot.Size = new System.Drawing.Size(70, 30);
+            this.bt_snapshot.TabIndex = 29;
+            this.bt_snapshot.Text = "截圖";
+            this.bt_snapshot.UseVisualStyleBackColor = true;
+            this.bt_snapshot.Click += new System.EventHandler(this.bt_snapshot_Click);
+            // 
+            // bt_start
+            // 
+            this.bt_start.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.bt_start.Location = new System.Drawing.Point(6, 20);
+            this.bt_start.Name = "bt_start";
+            this.bt_start.Size = new System.Drawing.Size(70, 30);
+            this.bt_start.TabIndex = 27;
+            this.bt_start.Text = "啟動";
+            this.bt_start.UseVisualStyleBackColor = true;
+            this.bt_start.Click += new System.EventHandler(this.bt_start_Click);
+            // 
+            // timer_display
+            // 
+            this.timer_display.Tick += new System.EventHandler(this.timer_display_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 261);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.bt_clear);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "imsCamera";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lb_fps;
+        private System.Windows.Forms.Timer timer_fps;
+        private System.Windows.Forms.Button bt_record_start;
+        private System.Windows.Forms.Button bt_record_stop;
+        private System.Windows.Forms.Button bt_clear;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bt_refresh;
+        private System.Windows.Forms.Button bt_exit;
+        private System.Windows.Forms.Button bt_stop;
+        private System.Windows.Forms.Button bt_snapshot;
+        private System.Windows.Forms.Button bt_start;
+        private System.Windows.Forms.Timer timer_display;
+        private System.Windows.Forms.Label lb_main_mesg;
+    }
+}
+
---
+++ _2.vcs/my_vcs_lesson_6/_Video/vcs_WebCam4_MotionDetection/vcs_WebCam4_MotionDetection/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Drawing.Imaging;   //for ImageFormat
+
+using AForge.Video;             //需要添加這兩個.dll, 參考/右鍵/加入參考/瀏覽 此二檔 AForge.Video.dll和AForge.Video.DirectShow.dll
+using AForge.Video.DirectShow;
+using AForge.Vision.Motion;     // Motion detection
+
+/*
+參考
+【AForge.NET】C#上使用AForge.Net擷取視訊畫面
+https://ccw1986.blogspot.com/2013/01/ccaforgenetcapture-image.html
+
+AForge下載鏈結
+http://www.aforgenet.com/framework/downloads.html
+
+Aforge.Net 安裝路徑設定
+Solution Explorer(方案總管) => References(參考)(右鍵) => Add Reference(加入參考) => AForge.Net的Release資料夾
+加入AForge.Video.dll、AForge.Video.DirectShow.dll
+
+移動偵測 需要 參考/加入參考/選取以下3個dll
+AForge.dll
+AForge.Imaging.dll
+AForge.Vision.dll
+*/
+
+namespace vcs_WebCam4_MotionDetection  // 標準 移動偵測
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection USBWebcams = null;
+        private VideoCaptureDevice Cam = null; // refrence to the actual VidioCaptureDevice (webcam)
+        MotionDetector motion_detector;
+
+        private const int BORDER = 10;
+
+        bool flag_motion_detection = false;
+
+        bool motionDetected = false; // was there any motion detected previously
+        int calibrateAndResume = 0; // counter used delay/skip frames from being processed by the MotionDetector
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            show_item_location();
+
+            Init_WebcamSetup();
+            Start_Webcam();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Stop_Webcam();
+            if (Cam != null)
+            {
+                if (Cam.IsRunning)  // When Form1 closes itself, WebCam must stop, too.
+                {
+                    Cam.Stop();   // WebCam stops capturing images.
+                    Cam.SignalToStop();
+                    Cam.WaitForStop();
+                }
+            }
+        }
+
+        void show_item_location()
+        {
+            int W = 640;
+            int H = 480;
+            int x_st = BORDER;
+            int y_st = BORDER;
+            int dx = W + 50;
+            int dy = H + 50;
+
+            pictureBox1.Size = new Size(W, H);
+
+            pictureBox1.Location = new Point(x_st + dx * 0, y_st + dy * 0);
+            bt_motion_detection.Location = new Point(x_st + W + BORDER, 10);
+            richTextBox1.Size = new Size(250, H - 40);
+            richTextBox1.Location = new Point(x_st + W + BORDER, 50);
+
+            this.ClientSize = new Size(W + 250 + BORDER * 3, H + BORDER * 2);
+        }
+
+        void Init_WebcamSetup()
+        {
+            USBWebcams = new FilterInfoCollection(FilterCategory.VideoInputDevice); //實例化對象
+
+            if (USBWebcams.Count > 0)
+            {
+                this.pictureBox1.Paint += new PaintEventHandler(DrawMessage);
+
+                //初始化motion detector
+                motion_detector = new MotionDetector(new TwoFramesDifferenceDetector(), new MotionAreaHighlighting());
+
+                Cam = new VideoCaptureDevice(USBWebcams[0].MonikerString);//長名
+                Cam.NewFrame += new NewFrameEventHandler(Cam_NewFrame); // defines which method to call when a new frame arrives
+            }
+        }
+
+        private void DrawMessage(object sender, PaintEventArgs e)
+        {
+            using (Font f = new Font("Arial", 14, FontStyle.Bold))
+            {
+                string str = string.Empty;
+                SolidBrush sb;
+                if (this.motionDetected == true)
+                {
+                    str = DateTime.Now.ToString() + " 移動偵測";
+                    sb = new SolidBrush(Color.Red);
+                    e.Graphics.DrawRectangle(new Pen(Color.Red, 10), 5, 5, 640 - 10, 480 - 10);
+                }
+                else
+                {
+                    str = DateTime.Now.ToString();
+                    sb = new SolidBrush(Color.Green);
+                }
+                e.Graphics.DrawString(str, f, sb, new Point(10, 10));
+            }
+        }
+
+        //自定義函數, 捕獲每一幀圖像並顯示
+        void Cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap1 = (Bitmap)eventArgs.Frame.Clone(); // get a copy of the BitMap from the VideoCaptureDevice
+            pictureBox1.Image = (Bitmap)bitmap1.Clone(); // displays the current frame on the main form
+
+            if ((this.flag_motion_detection == true) && (this.motionDetected == false))
+            {
+                // if motion detection is enabled and there werent any previous motion detected
+                Bitmap bitmap2 = (Bitmap)bitmap1.Clone(); // clone the bits from the current frame
+
+                if (motion_detector.ProcessFrame(bitmap2) > 0.001) // feed the bits to the MD 
+                {
+                    if (this.calibrateAndResume > 3)
+                    {
+                        // if motion was detected in 3 subsequent frames
+                        Thread th = new Thread(MotionReaction);
+                        th.Start(); // start the motion reaction thread
+                    }
+                    else
+                    {
+                        this.calibrateAndResume++;
+                    }
+                }
+            }
+        }
+
+        private void MotionReaction()
+        {
+            this.motionDetected = true;
+
+            Thread.Sleep(10000); // the user is notified for 10 seconds
+            calibrateAndResume = 0;
+            this.motionDetected = false;
+            Thread.Sleep(3000);
+        }
+
+        void Start_Webcam()
+        {
+            if (Cam != null)
+            {
+                Cam.Start();   // WebCam starts capturing images.
+            }
+        }
+
+        void Stop_Webcam()
+        {
+            if (Cam != null)
+            {
+                //show_main_message("停止", S_OK, 20);
+                Cam.Stop();  // WebCam stops capturing images.
+                Cam.SignalToStop();
+                Cam.WaitForStop();
+                while (Cam.IsRunning)
+                {
+                    Console.Write("等候相機關閉");
+                }
+                Cam = null;
+            }
+            pictureBox1.Image = null;
+        }
+
+        private void bt_motion_detection_Click(object sender, EventArgs e)
+        {
+            if (flag_motion_detection == false)
+            {
+                flag_motion_detection = true;
+                richTextBox1.Text += "啟動 移動偵測\n";
+                bt_motion_detection.Text = "停止 移動偵測";
+            }
+            else
+            {
+                flag_motion_detection = false;
+                richTextBox1.Text += "停止 移動偵測\n";
+                bt_motion_detection.Text = "啟動 移動偵測";
+            }
+        }
+    }
+}
