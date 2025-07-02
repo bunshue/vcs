e25b6d42938950ef3911b16abf838f32fc71948d
david <david@insighteyes.com>
2025-07-02 19:57:25 +0800

    david modify vcs code

--- _4.python/opencv/opencv3_face/1.haar_cascade_classifier/Haar_CascadeClassifier01.py
+++ _4.python/opencv/opencv3_face/1.haar_cascade_classifier/Haar_CascadeClassifier01.py
@@ -640,6 +640,34 @@ cv2.destroyAllWindows()
 print("------------------------------------------------------------")  # 60個
 print("------------------------------------------------------------")  # 60個
 
+pictPath = "haar_carplate.xml"  # 哈爾特徵檔路徑
+
+img = cv2.imread("testCar/cartest1.jpg")
+
+car_cascade = cv2.CascadeClassifier(pictPath)  # 讀哈爾特徵檔
+
+# 執行辨識
+plates = car_cascade.detectMultiScale(
+    img, scaleFactor=1.05, minNeighbors=3, minSize=(20, 20), maxSize=(155, 50)
+)
+if len(plates) > 0:  # 有偵測到車牌
+    for x, y, w, h in plates:  # 標記車牌
+        carplate = img[y : y + h, x : x + w]  # 車牌影像
+        cv2.rectangle(img, (x, y), (x + w, y + h), RED, 2)
+else:
+    print("偵測車牌失敗")
+
+cv2.imshow("Car1", img)  # 顯示所讀取的車輛
+cv2.imshow("Car2", carplate)  # 顯示所讀取的車輛
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+sys.exit()
+
+print("------------------------------------------------------------")  # 60個
+print("------------------------------------------------------------")  # 60個
+
 # 偵測車牌, 適用於俄羅斯車牌 haarcascade_russian_plate_number.xml
 
 russian_plate_cascade_classifier = cv2.CascadeClassifier(
@@ -848,6 +876,156 @@ cv2.imshow("ImageShow", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 """
+print("------------------------------------------------------------")  # 60個
+print("------------------------------------------------------------")  # 60個
+
+
+import os
+import cv2
+import numpy as np
+import pytesseract
+from PIL import Image
+
+print("------------------------------------------------------------")  # 60個
+
+# ch31_2.py
+
+config = '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
+text = pytesseract.image_to_string(Image.open("data/atq9305.jpg"), config=config)
+print(f"車號是 : {text}")
+
+print("------------------------------------------------------------")  # 60個
+print("------------------------------------------------------------")  # 60個
+
+# ch31_3.py
+
+config = '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
+pictPath = "haar_carplate.xml"  # 哈爾特徵檔路徑
+
+img = cv2.imread("car_plate/cartest1.jpg")
+
+car_cascade = cv2.CascadeClassifier(pictPath)  # 讀哈爾特徵檔
+
+# 執行辨識
+plates = car_cascade.detectMultiScale(
+    img, scaleFactor=1.05, minNeighbors=3, minSize=(20, 20), maxSize=(155, 50)
+)
+if len(plates) > 0:  # 有偵測到車牌
+    for x, y, w, h in plates:  # 標記車牌
+        carplate = img[y : y + h, x : x + w]  # 車牌影像
+else:
+    print("偵測車牌失敗")
+
+cv2.imshow("Car", carplate)  # 顯示所讀取的車輛
+text = pytesseract.image_to_string(carplate, config=config)  # OCR辨識
+print(f"車號是 : {text}")
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print("------------------------------------------------------------")  # 60個
+print("------------------------------------------------------------")  # 60個
+
+# ch31_4.py
+
+config = '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
+pictPath = "haar_carplate.xml"  # 哈爾特徵檔路徑
+
+img = cv2.imread("car_plate/cartest3.jpg")
+
+car_cascade = cv2.CascadeClassifier(pictPath)  # 讀哈爾特徵檔
+
+# 執行辨識
+plates = car_cascade.detectMultiScale(
+    img, scaleFactor=1.05, minNeighbors=3, minSize=(20, 20), maxSize=(155, 50)
+)
+if len(plates) > 0:  # 有偵測到車牌
+    for x, y, w, h in plates:  # 標記車牌
+        carplate = img[y : y + h, x : x + w]  # 車牌影像
+else:
+    print("偵測車牌失敗")
+
+cv2.imshow("Car", carplate)  # 顯示所讀取的車輛
+
+text = pytesseract.image_to_string(carplate, config=config)  # OCR辨識
+print(f"車號是 : {text}")
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print("------------------------------------------------------------")  # 60個
+print("------------------------------------------------------------")  # 60個
+
+# ch31_5.py
+
+carFile = "car_plate.jpg"
+config = '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
+pictPath = "haar_carplate.xml"  # 哈爾特徵檔路徑
+
+img = cv2.imread("car_plate/cartest3.jpg")
+
+car_cascade = cv2.CascadeClassifier(pictPath)  # 讀哈爾特徵檔
+
+# 執行辨識
+plates = car_cascade.detectMultiScale(
+    img, scaleFactor=1.05, minNeighbors=3, minSize=(20, 20), maxSize=(155, 50)
+)
+if len(plates) > 0:  # 有偵測到車牌
+    for x, y, w, h in plates:  # 標記車牌
+        carplate = img[y : y + h, x : x + w]  # 車牌影像
+else:
+    print("偵測車牌失敗")
+
+cv2.imshow("Car", carplate)  # 顯示所讀取的車輛
+
+ret, dst = cv2.threshold(carplate, 100, 255, cv2.THRESH_BINARY)  # 二值化
+cv2.imshow("Car binary", dst)  # 顯示二值化車牌
+
+text = pytesseract.image_to_string(carplate, config=config)  # OCR辨識
+print(f"車號是 : {text}")
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print("------------------------------------------------------------")  # 60個
+print("------------------------------------------------------------")  # 60個
+
+# ch31_6.py
+
+carFile = "car_plate.jpg"
+config = '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
+pictPath = "haar_carplate.xml"  # 哈爾特徵檔路徑
+
+img = cv2.imread("car_plate/cartest3.jpg")
+
+car_cascade = cv2.CascadeClassifier(pictPath)  # 讀哈爾特徵檔
+
+# 執行辨識
+plates = car_cascade.detectMultiScale(
+    img, scaleFactor=1.05, minNeighbors=3, minSize=(20, 20), maxSize=(155, 50)
+)
+if len(plates) > 0:  # 有偵測到車牌
+    for x, y, w, h in plates:  # 標記車牌
+        carplate = img[y : y + h, x : x + w]  # 車牌影像
+else:
+    print("偵測車牌失敗")
+
+cv2.imshow("Car", carplate)  # 顯示所讀取的車輛
+
+ret, dst = cv2.threshold(carplate, 100, 255, cv2.THRESH_BINARY)  # 二值化
+cv2.imshow("Car binary", dst)  # 顯示二值化車牌
+
+kernel = np.ones((3, 3), np.uint8)
+dst1 = cv2.morphologyEx(dst, cv2.MORPH_OPEN, kernel)  # 執行開運算
+text = pytesseract.image_to_string(dst1, config=config)  # 執行辨識
+print(f"車號是 : {text}")
+
+cv2.imwrite(carFile, dst)  # 寫入儲存
+print("已存圖, 檔案 :", carFile)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 print("------------------------------------------------------------")  # 60個
 print("------------------------------------------------------------")  # 60個
 
@@ -1261,3 +1439,6 @@ print("------------------------------------------------------------")  # 60個
 
 print("------------------------------------------------------------")  # 60個
 print("------------------------------------------------------------")  # 60個
+
+cv2.imwrite("tmp_atq9305.jpg", carplate)
+print("已存圖, 檔案 : tmp_atq9305.jpg")
