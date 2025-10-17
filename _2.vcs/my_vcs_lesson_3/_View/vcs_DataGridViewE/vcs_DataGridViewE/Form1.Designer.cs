f347707f536c3e667dad1c6c7d9bc053d0edfb2f
david <david@insighteyes.com>
2025-10-17 17:16:28 +0800

    david modify vcs code

--- _2.vcs/my_vcs_lesson_3/_View/vcs_DataGridViewE/vcs_DataGridViewE/Form1.Designer.cs
+++ _2.vcs/my_vcs_lesson_3/_View/vcs_DataGridViewE/vcs_DataGridViewE/Form1.Designer.cs
@@ -39,13 +39,21 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(466, 30);
+            this.dataGridView1.Location = new System.Drawing.Point(308, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(150, 150);
@@ -53,7 +61,7 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(466, 186);
+            this.richTextBox1.Location = new System.Drawing.Point(308, 168);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(150, 150);
             this.richTextBox1.TabIndex = 1;
@@ -62,7 +70,7 @@
             // bt_clear
             // 
             this.bt_clear.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.bt_clear.Location = new System.Drawing.Point(514, 272);
+            this.bt_clear.Location = new System.Drawing.Point(384, 272);
             this.bt_clear.Name = "bt_clear";
             this.bt_clear.Size = new System.Drawing.Size(60, 32);
             this.bt_clear.TabIndex = 34;
@@ -143,7 +151,7 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(130, 60);
             this.button6.TabIndex = 41;
-            this.button6.Text = "清除DGV資料";
+            this.button6.Text = "建立DGV 6";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
@@ -154,15 +162,111 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(130, 60);
             this.button7.TabIndex = 42;
-            this.button7.Text = "顯示DGV內容";
+            this.button7.Text = "建立DGV 7";
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button8.Location = new System.Drawing.Point(148, 12);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(130, 60);
+            this.button8.TabIndex = 43;
+            this.button8.Text = "建立DGV 8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button9.Location = new System.Drawing.Point(148, 70);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(130, 60);
+            this.button9.TabIndex = 44;
+            this.button9.Text = "建立DGV 9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button10.Location = new System.Drawing.Point(148, 128);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(130, 60);
+            this.button10.TabIndex = 45;
+            this.button10.Text = "建立DGV 10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button11.Location = new System.Drawing.Point(148, 186);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(130, 60);
+            this.button11.TabIndex = 46;
+            this.button11.Text = "DGV 隔行換色";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button12.Location = new System.Drawing.Point(148, 244);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(130, 60);
+            this.button12.TabIndex = 47;
+            this.button12.Text = "DGV 刪除";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button13.Location = new System.Drawing.Point(148, 310);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(130, 60);
+            this.button13.TabIndex = 48;
+            this.button13.Text = "DGV 跳至最後一行顯示";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button14.Location = new System.Drawing.Point(148, 376);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(130, 60);
+            this.button14.TabIndex = 49;
+            this.button14.Text = "清除DGV資料";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button15.Location = new System.Drawing.Point(148, 442);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(130, 60);
+            this.button15.TabIndex = 50;
+            this.button15.Text = "顯示DGV內容";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(715, 517);
+            this.ClientSize = new System.Drawing.Size(552, 546);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -195,6 +299,14 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
     }
 }
 
--- _2.vcs/my_vcs_lesson_3/_View/vcs_DataGridViewE/vcs_DataGridViewE/Form1.cs
+++ _2.vcs/my_vcs_lesson_3/_View/vcs_DataGridViewE/vcs_DataGridViewE/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using System.Data.OleDb;
+
 namespace vcs_DataGridViewE
 {
     public partial class Form1 : Form
@@ -19,7 +21,41 @@ namespace vcs_DataGridViewE
         private void Form1_Load(object sender, EventArgs e)
         {
             show_item_location();
+            dataGridView1.CellMouseClick += new DataGridViewCellMouseEventHandler(dataGridView1_CellMouseClick);
+            dataGridView1.RowHeaderMouseClick += new DataGridViewCellMouseEventHandler(dataGridView1_RowHeaderMouseClick);
         }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            //標題行上點擊右鍵，出現快捷菜單。
+            //只有在 CellMouseClick 事件中才能響應右鍵
+            // 判斷是否右鍵點擊
+            if (e.Button == MouseButtons.Right)
+            {
+                // 得到點擊所在的行和列信息。相關函數查 MSDN
+                DataGridView.HitTestInfo hitinfo = dataGridView1.HitTest(e.X, e.Y);
+                // 如果 RowIndex < 0,就是標題行了。 
+                if (hitinfo.RowIndex < 0)
+                {
+                    // 如果你只要指定的列顯示菜單，則加入對 hitinfo.ColumnIndex 的判斷
+                    //contextMenuStrip1.Show(MousePosition.X, MousePosition.Y);
+                    richTextBox1.Text += "標題行上點擊右鍵，出現快捷菜單\n";
+                }
+            }
+
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                //加入顯示右鍵彈出菜單
+                richTextBox1.Text += "標題行上點擊右鍵，出現快捷菜單   無用\n";
+            }
+        }
+
+
+
 
         void show_item_location()
         {
@@ -42,16 +78,24 @@ namespace vcs_DataGridViewE
             button5.Location = new Point(x_st + dx * 0, y_st + dy * 5);
             button6.Location = new Point(x_st + dx * 0, y_st + dy * 6);
             button7.Location = new Point(x_st + dx * 0, y_st + dy * 7);
+            button8.Location = new Point(x_st + dx * 1, y_st + dy * 0);
+            button9.Location = new Point(x_st + dx * 1, y_st + dy * 1);
+            button10.Location = new Point(x_st + dx * 1, y_st + dy * 2);
+            button11.Location = new Point(x_st + dx * 1, y_st + dy * 3);
+            button12.Location = new Point(x_st + dx * 1, y_st + dy * 4);
+            button13.Location = new Point(x_st + dx * 1, y_st + dy * 5);
+            button14.Location = new Point(x_st + dx * 1, y_st + dy * 6);
+            button15.Location = new Point(x_st + dx * 1, y_st + dy * 7);
 
-            dataGridView1.Size = new Size(400, 540);
-            dataGridView1.Location = new Point(x_st + dx * 1, y_st + dy * 0);
+            dataGridView1.Size = new Size(600, 320);
+            dataGridView1.Location = new Point(x_st + dx * 2, y_st + dy * 0);
 
-            richTextBox1.Size = new Size(400, 540);
-            richTextBox1.Location = new Point(x_st + dx * 4, y_st + dy * 0);
+            richTextBox1.Size = new Size(600, 320);
+            richTextBox1.Location = new Point(x_st + dx * 2, y_st + dy * 5);
 
             bt_clear.Location = new Point(richTextBox1.Location.X + richTextBox1.Size.Width - bt_clear.Size.Width, richTextBox1.Location.Y + richTextBox1.Size.Height - bt_clear.Size.Height);
 
-            this.Size = new Size(1000, 620);
+            this.Size = new Size(920, 740);
         }
 
         private void bt_clear_Click(object sender, EventArgs e)
@@ -198,28 +242,261 @@ namespace vcs_DataGridViewE
 
         private void button3_Click(object sender, EventArgs e)
         {
+            //List oledb providers in DataGridView
+            fill_DataGridView();
         }
 
+        void fill_DataGridView()
+        {
+            OleDbEnumerator enumerator = new OleDbEnumerator();
+            dataGridView1.DataSource = enumerator.GetElements();
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            }
+        }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            dataGridView1.ColumnCount = 2;
+            dataGridView1.Columns[0].Name = "品名";
+            dataGridView1.Columns[1].Name = "單價";
 
+            dataGridView1.Rows.Add(new Object[] { "紅茶", 25 });
+            dataGridView1.Rows.Add(new Object[] { "綠茶", 25 });
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;  //設置如何選中單元格 整行一起選取
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
+            //綁定數據集合
+            dataGridView1.DataSource = new List<Fruit>()
+            {
+            new Fruit(){Name="蘋果",Price=30},
+            new Fruit(){Name="橘子",Price=40},
+            new Fruit(){Name="梨子",Price=33},
+            new Fruit(){Name="水蜜桃",Price=31}
+            };
+            dataGridView1.Columns[0].Width = 200;
+            dataGridView1.Columns[1].Width = 170;
 
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;  //設置如何選中單元格 整行一起選取
+            dataGridView1.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter; //設置對其方式   此欄置中對齊
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            //清除DGV資料
-            dataGridView1.Rows.Clear();
+            load_data();
+
+
+            //int index = dataGridView1.Rows.Add();
+            //dataGridView1.Rows[index].Cells[0].Value = "123456";
+
+            string[] row0 = { "11/22/1968", "29", "Revolution 9", "Beatles", "The Beatles [White Album]" };
+            dataGridView1.Rows.Add(row0);
+
+            //DataGridViewRowCollection rows = dataGridView1.Rows;
+            //rows.Add(new Object[] { "11/22/1968", "29", "Revolution 9", "Beatles", "The Beatles [White Album]" });
+
         }
+
+        void load_data()
+        {
+            SetupDataGridView();        //設定DGV
+            PopulateDataGridView();     //填入資料
+        }
+
+        private void SetupDataGridView()
+        {
+            dataGridView1.ColumnCount = 5;
+
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.Navy;
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font(dataGridView1.Font, FontStyle.Bold);
+
+            dataGridView1.Name = "dataGridView1";
+            //dataGridView1.Location = new Point(8, 8);
+            //dataGridView1.Size = new Size(500, 250);
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.Single;
+            dataGridView1.GridColor = Color.Black;
+            dataGridView1.RowHeadersVisible = false;
+
+            dataGridView1.Columns[0].Name = "Release Date";
+            dataGridView1.Columns[1].Name = "Track";
+            dataGridView1.Columns[2].Name = "Title";
+            dataGridView1.Columns[3].Name = "Artist";
+            dataGridView1.Columns[4].Name = "Album";
+            dataGridView1.Columns[4].DefaultCellStyle.Font = new Font(dataGridView1.DefaultCellStyle.Font, FontStyle.Italic);
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;  //設置如何選中單元格 整行一起選取
+            dataGridView1.MultiSelect = false;
+            dataGridView1.Dock = DockStyle.Fill;
+
+            //dataGridView1.CellFormatting += new DataGridViewCellFormattingEventHandler(songsDataGridView_CellFormatting);
+        }
+
+        private void PopulateDataGridView()
+        {
+            string[] row0 = { "11/22/1968", "29", "Revolution 9", "Beatles", "The Beatles [White Album]" };
+            string[] row1 = { "1960", "6", "Fools Rush In", "Frank Sinatra", "Nice 'N' Easy" };
+            string[] row2 = { "11/11/1971", "1", "One of These Days", "Pink Floyd", "Meddle" };
+            string[] row3 = { "1988", "7", "Where Is My Mind?", "Pixies", "Surfer Rosa" };
+            string[] row4 = { "5/1981", "9", "Can't Find My Mind", "Cramps", "Psychedelic Jungle" };
+            string[] row5 = { "6/10/2003", "13", "Scatterbrain. (As Dead As Leaves.)", "Radiohead", "Hail to the Thief" };
+            string[] row6 = { "6/30/1992", "3", "Dress", "P J Harvey", "Dry" };
+
+            int i;
+            for (i = 0; i < 10; i++)
+            {
+                dataGridView1.Rows.Add(row0);
+                dataGridView1.Rows.Add(row1);
+                dataGridView1.Rows.Add(row2);
+                dataGridView1.Rows.Add(row3);
+                dataGridView1.Rows.Add(row4);
+                dataGridView1.Rows.Add(row5);
+                dataGridView1.Rows.Add(row6);
+            }
+
+            /*
+            //dataGridView 顯示欄排序
+            dataGridView1.Columns[0].DisplayIndex = 3;
+            dataGridView1.Columns[1].DisplayIndex = 4;
+            dataGridView1.Columns[2].DisplayIndex = 0;
+            dataGridView1.Columns[3].DisplayIndex = 1;
+            dataGridView1.Columns[4].DisplayIndex = 2;
+            */
+        }
+
 
         private void button7_Click(object sender, EventArgs e)
         {
+            richTextBox1.Text += "在DataGridView控件做下拉式列表\n";
+            //創建列對象
+            DataGridViewComboBoxColumn dgvc = new DataGridViewComboBoxColumn();
+            dgvc.Items.Add("蘋果");//向集合中添加元素
+            dgvc.Items.Add("芒果");//向集合中添加元素
+            dgvc.Items.Add("鴨梨");//向集合中添加元素
+            dgvc.Items.Add("橘子");//向集合中添加元素
+            dgvc.HeaderText = "水果";//設置列標題文本
+            dataGridView1.Columns.Add(dgvc);//將列添加到集合
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //DT轉DGV
+            richTextBox1.Text += "將List的資料連結到 DataGridView 裏\n";
+
+            List<Point> Points = new List<Point>();
+            Points.Clear();
+
+            int i;
+            Random r = new Random();
+            for (i = 0; i < 10; i++)
+            {
+                int i_st = r.Next(10);
+                int j_st = r.Next(10);
+                Points.Add(new Point(i_st, j_st));
+                richTextBox1.Text += "i_st = " + i_st.ToString() + "\t" + "j_st = " + j_st.ToString() + "\n";
+            }
+
+            printList(Points);
+
+            dataGridView1.DataSource = Points;
+
+        }
+
+        void printList(List<Point> pts)
+        {
+            int len = pts.Count;
+            richTextBox1.Text += "len = " + len.ToString() + "\n";
+            int i;
+            for (i = 0; i < len; i++)
+            {
+                richTextBox1.Text += pts[i].ToString() + "\n";
+            }
+        }
+
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //DGV 隔行換色
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.LightYellow;   //隔行更換背景色
+                }
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //DGV 刪除
+            if (this.dataGridView1.SelectedRows.Count > 0 && this.dataGridView1.SelectedRows[0].Index != this.dataGridView1.Rows.Count - 1)
+            {
+                this.dataGridView1.Rows.RemoveAt(this.dataGridView1.SelectedRows[0].Index);
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //DGV 跳至最後一行顯示
+            dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.RowCount - 1;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            //清除DGV資料
+            dataGridView1.Rows.Clear();         //刪除row資料, 留下標題
+
+            //dataGridView1.Columns.Clear();    //刪除標題
+
+            //clear
+            //dataGridView1.DataSource = null;
+            //dataGridView1.Invalidate();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
             //顯示DGV內容
+            print_dataGridView_data(dataGridView1);
+        }
+
+        void print_dataGridView_data(DataGridView dgv)
+        {
+            int r;
+            int c;
+            int rows = dgv.RowCount;
+            int cols = dgv.ColumnCount;
+            richTextBox1.Text += "ROWS = " + rows.ToString() + "\n";
+            richTextBox1.Text += "COLS = " + cols.ToString() + "\n";
+            richTextBox1.Text += "Content:\n";
+
+            for (r = 0; r < rows; r++)
+            {
+                richTextBox1.Text += "r = " + r.ToString() + "\t";
+                for (c = 0; c < cols; c++)
+                {
+                    //richTextBox1.Text += dataGridView1[c, r].Value + "\t";
+                    DataGridViewCell dgvCell = dgv[c, r];
+                    richTextBox1.Text += dgvCell.Value + "\t";
+                }
+                richTextBox1.Text += "\n";
+            }
+            richTextBox1.Text += "\n";
         }
     }
 }
