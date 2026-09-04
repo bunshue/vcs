518bd92acc83f871d540dd1a22ad3e105fa46370
david <david@insighteyes.com>
2026-09-04 18:12:29 +0800

    david modify vcs code

--- _2.vcs/my_vcs_lesson_3/vcs_PictureBox3_Resizable2/vcs_PictureBox3_Resizable2/Form1.cs
+++ _2.vcs/my_vcs_lesson_3/vcs_PictureBox3_Resizable2/vcs_PictureBox3_Resizable2/Form1.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 
 //預設是不能用滑鼠直接拉動大小的，需要自己加上「滑鼠事件」來模擬「可調整大小」的行為
 
-namespace vcs_PictureBox3_Resizable
+namespace vcs_PictureBox3_Resizable2
 {
     public partial class Form1 : Form
     {
@@ -25,21 +25,46 @@ namespace vcs_PictureBox3_Resizable
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Text = "Resizable & Movable PictureBox Demo";
-            this.Size = new Size(800, 600);
+            show_item_location();
 
-            pictureBox = new PictureBox();
-            pictureBox.BackColor = Color.LightBlue;
-            pictureBox.Location = new Point(100, 100);
-            pictureBox.Size = new Size(200, 150);
+            //------------------------------------------------------------  # 60個
 
-            // 加入事件
-            pictureBox.MouseDown += PictureBox_MouseDown;
-            pictureBox.MouseMove += PictureBox_MouseMove;
-            pictureBox.MouseUp += PictureBox_MouseUp;
-
-            this.Controls.Add(pictureBox);
+            string filename = @"D:\_git\vcs\_1.data\______test_files1\elephant.jpg";
+            pictureBox1.ImageLocation = filename;
         }
+
+        void show_item_location()
+        {
+            //button
+            int x_st = 10;
+            int y_st = 10;
+            int dx = 200 + 10;
+            int dy = 60 + 10;
+
+            pictureBox1.Size = new Size(840, 560);
+            pictureBox1.Location = new Point(x_st + dx * 0, y_st + dy * 0);
+
+            button0.Location = new Point(x_st + dx * 4 + 100, y_st + dy * 0);
+            button1.Location = new Point(x_st + dx * 4 + 100, y_st + dy * 1);
+            richTextBox1.Size = new Size(300, 690 - 140);
+            richTextBox1.Location = new Point(x_st + dx * 4 + 100, y_st + dy * 2);
+            bt_clear.Location = new Point(richTextBox1.Location.X + richTextBox1.Size.Width - bt_clear.Size.Width, richTextBox1.Location.Y + richTextBox1.Size.Height - bt_clear.Size.Height);
+
+
+            this.Size = new Size(1273, 750);
+            this.Text = "vcs_test_all_00_Usually";
+
+            //設定執行後的表單起始位置, 正中央
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point((Screen.PrimaryScreen.Bounds.Width - this.Size.Width) / 2, (Screen.PrimaryScreen.Bounds.Height - this.Size.Height) / 2);
+        }
+
+        private void bt_clear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        //------------------------------------------------------------  # 60個
 
         private void PictureBox_MouseDown(object sender, MouseEventArgs e)
         {
@@ -102,6 +127,30 @@ namespace vcs_PictureBox3_Resizable
             resizing = false;
             moving = false;
             pictureBox.Cursor = Cursors.Default;
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            //選取
+
+            pictureBox = new PictureBox();
+            pictureBox.BackColor = Color.Transparent;
+            pictureBox.Location = new Point(100, 100);
+            pictureBox.Size = new Size(200, 150);
+
+            // 加入事件
+            pictureBox.MouseDown += PictureBox_MouseDown;
+            pictureBox.MouseMove += PictureBox_MouseMove;
+            pictureBox.MouseUp += PictureBox_MouseUp;
+
+            this.Controls.Add(pictureBox);
+            pictureBox.BringToFront();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //確定
+
         }
     }
 }
